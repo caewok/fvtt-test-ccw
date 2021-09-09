0abbf5775b4be24c725d20451b766e21e9e6e766
Michael Enion <caewok_2000@yahoo.com>
2021-09-09 15:59:48 -0700

    Module framework for testing.

---
+++ scripts/patching.js
@@ -0,0 +1,5 @@
+import { MODULE_ID } from "./module.js";
+
+export function registerCCW() {
+//   libWrapper.register(MODULE_ID, 'window.libRuler.RulerSegment.distanceFunction', distanceFunctionManhattan, 'OVERRIDE');
+}
