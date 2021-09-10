0575e5e7909b388d057e4b9a875dd5f678fa0fe0
Michael Enion <caewok_2000@yahoo.com>
2021-09-10 12:17:54 -0700

    Add wall ccw methods

--- scripts/patching.js
+++ scripts/patching.js
@@ -3,8 +3,26 @@ import { testCCWInitializeEndpoints,
          testCCWIncludeWall,
          testCCWSweepEndpoints } from "./radial_sweep.js";
 
+import { wallCCW, wallWhichSide } from "./wall.js";
+
 export function registerCCW() {
   libWrapper.register(MODULE_ID, 'RadialSweepPolygon.prototype._initializeEndpoints', testCCWInitializeEndpoints, 'MIXED');
   libWrapper.register(MODULE_ID, 'RadialSweepPolygon.prototype._sweepEndpoints', testCCWSweepEndpoints, 'MIXED');
   libWrapper.register(MODULE_ID, 'RadialSweepPolygon.prototype._includeWall', testCCWIncludeWall, 'MIXED');
 }
+
+// Add new properties to existing classes
+
+// ---------------- Wall ------------- // 
+
+Object.defineProperty(Wall.prototype, "ccw", {
+  value: wallCCW,
+  writable: true,
+  configurable: true
+});
+
+Object.defineProperty(Wall.prototype, "whichSide", {
+  value: wallWhichSide,
+  writable: true,
+  configurable: true
+});
---
+++ scripts/wall.js
@@ -0,0 +1,35 @@
+// Additions to the wall class
+import { orient2d } from "./lib/orient2d.min.js";
+
+/*
+ * Wall.prototype.ccw
+ *
+ * Check if the wall is counter-clockwise or clockwise in relation to an origin point.
+ * origin --> first wall coord --> second wall coord
+ * See whichSide method.
+ * 
+ * @param {PIXI.point} origin   PIXI.point or other object with {x, y}.
+ * @return {Number} Negative if counter-clockwise, Positive if clockwise, 0 if origin is in line with the wall
+ */ 
+export function wallCCW(origin) {
+  const c = this.coords;
+  retur orient2d(origin.x, origin.y,
+           c[0], c[1],
+           c[2], c[3]);
+}
+
+
+/*
+ * Wall.prototype.whichSide
+ *
+ * Report the side of the origin in relation to the wall, using ccw algorithm.
+ * 
+ * @param {PIXI.point} origin   PIXI.point or other object with {x, y}.
+ * @return {Number} -1 if left, 1 if right, 0 if origin is in line with the wall
+ */
+export function wallWhichSide(origin) {
+  const orientation = this.ccw(origin);
+  
+  return orientation < 0 ? -1 : 
+         orientation > 0 ?  1 : 0;
+}
