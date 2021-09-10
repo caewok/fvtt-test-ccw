ff7d99967af440c72807d6e0831b50b7af17549a
Michael Enion <caewok_2000@yahoo.com>
2021-09-09 17:25:18 -0700

    Wrap likely functions and set up benchmark function.

--- scripts/patching.js
+++ scripts/patching.js
@@ -1,5 +1,9 @@
 import { MODULE_ID } from "./module.js";
+import { testCCWInitializeEndpoints, 
+         testCCWIncludeWall,
+         testCCWSweepEndpoints } from "./radial_sweep.js";
 
 export function registerCCW() {
-//   libWrapper.register(MODULE_ID, 'window.libRuler.RulerSegment.distanceFunction', distanceFunctionManhattan, 'OVERRIDE');
+  libWrapper.register(MODULE_ID, 'RadialSweep.prototype._initializeEndpoints', testCCWInitializeEndpoints, 'MIXED');
+  libWrapper.register(MODULE_ID, 'RadialSweep.prototype._sweepEndpoints', testCCWSweepEndpoints, 'MIXED');
 }
