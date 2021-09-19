ab80ce1cc49ad27706834296f0ecbb0e3caed976
Michael Enion <caewok_2000@yahoo.com>
2021-09-19 07:15:18 -0700

    Add distanceSquaredMethod and cache the distance to origin.
    Make private origin and radius properties. Use setters to un-cache the dependent calculations.

--- scripts/class_CCWSweepPoint.js
+++ scripts/class_CCWSweepPoint.js
@@ -34,17 +34,19 @@ class CCWSweepPoint extends PIXI.Point {
     /**
      * Origin for the sweep.
      * Only required if insideRadius is used.
-     * @type {PIXI.Point}
+     * @type {x: number, y: number}
+     * @private
      */
-    this.origin = origin;
+    this._origin = origin;
     
     /**
      * Radius of the FOV.
      * Only required if insideRadius is used.
      * Should be strictly positive.
      * @type {number}
+     * @private
      */
-    this.radius = radius;
+    this._radius = radius;
     
     /**
      * Cache whether this point is inside the FOV radius.
@@ -52,11 +54,28 @@ class CCWSweepPoint extends PIXI.Point {
      * @private
      */
     this._insideRadius = undefined;
+    
+    /**
+     * Cache the distance squared to the origin
+     * @type {number}
+     * @private
+     */
+    this._distanceSquaredToOrigin = undefined;
   }
   
   /* -------------------------------------------- */
   /*  Getters / Setters                           */
   /* -------------------------------------------- */
+  
+  /*
+   * @param {number}
+   */
+  get radius() { return this._radius; }
+  
+  /*
+   * @param {x: number, y: number}
+   */
+  get origin() { return this._origin; }
   
   /* 
    * Is this point associated with a radius?
@@ -67,28 +86,65 @@ class CCWSweepPoint extends PIXI.Point {
   }
   
   /*
+   * Distance squared to origin. Used for comparisons.
+   */
+  get distanceSquaredToOrigin() {
+    if(this._distanceSquaredToOrigin = undefined) {
+      this._distanceSquaredToOrigin = distanceSquared(this.origin);
+    }
+    return this._distanceSquaredToOrigin;
+  }
+    
+  /*
    * Is this point inside the FOV radius?
    * @return {undefined|boolean}
    */
   get insideRadius() {
     if(!this.hasRadius) return undefined;
-    if(this._insideRadius === undefined) {
-      const dx = this.x - this.origin.x;
-      const dy = this.y - this.origin.y;
-    
-      this._insideRadius = (dx*dx + dy*dy) <= this.radius;
+    if(this._insideRadius === undefined) { 
+      this._insideRadius = this.distanceSquaredToOrigin <= (this.radius * this.radius);
     }
     return this._insideRadius;
   }
+  
+  /*
+   * When setting origin, un-cache measurements that depend on it.
+   * @param {x: number, y: number} value
+   */
+  set origin(value) {
+    this._origin = value;
+    this._distanceSquaredToOrigin = undefined;
+    this._insideRadius = undefined;
+  }
+  
+  /*
+   * When setting radius, un-cache measurements that depend on it.
+   * @param {number} value
+   */
+  set radius(value) {
+    this._radius = value;
+    this._insideRadius = undefined;
+  }
+   
 
   /* -------------------------------------------- */
   /*  Methods                                     */
   /* -------------------------------------------- */
   
+  /*
+   * Distance squared used for comparisons.
+   * @param {x: number, y: number}  p   Point to measure to
+   */
+  distanceSquared(p) {
+    const dx = this.x - p.x;
+    const dy = this.y - p.y;
+    return (dx*dx + dy*dy);
+  }
+  
   /**
    * Does this endpoint equal some other endpoint?
    * This version treats points equivalent if rounded values are equal
-   * @param {Point} other     Some other point with x and y coordinates
+   * @param {x: number, y: number} other     Some other point with x and y coordinates
    * @returns {boolean}       Are the points equal?
    */
   equals(other) {
