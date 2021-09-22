b713e2f8477e3b626d011c257bb825ba795bc70b
Michael Enion <caewok_2000@yahoo.com>
2021-09-21 17:29:59 -0700

    Remove all bezier cache code; not faster than without

--- debug/benchmarking.js
+++ debug/benchmarking.js
@@ -72,8 +72,92 @@ benchmarkLoop(10000, Poly, CCWSweepPolygon.prototype._initializeEndpoints, Poly.
 benchmarkLoop(10000, RadialPoly, RadialSweepPolygon.prototype._initializeEndpoints, Poly.config.type) // .077 ms
 
 
+function arraysEqual(a1,a2) {
+    /* WARNING: arrays must not contain {objects} or behavior may be undefined */
+    return JSON.stringify(a1)==JSON.stringify(a2);
+}
+
+function bezierCircle(t) {
+    // const BezierCache = game.modules.get(MODULE_ID).api.BezierCache;
+//     if(BezierCache.has(t)) { return BezierCache.get(t); }
+  
+    const paren = 1 - t;
+    const paren2 = paren * paren;
+    const paren3 = paren2 * paren;
+    const t2 = t * t;
+    const t3 = t * t * t;
+    const c_times_3 = 3 * 0.551915024494;
+  
+    const x = c_times_3 * paren2 * t + 3 * paren * t2 + t3;
+    const y = c_times_3 * t2 * paren + 3 * t * paren2 + paren3;  
+    
+//     BezierCache.set(t, {x: x, y: y});
+    
+    return { x: x, y: y };
+  }
+  
+Q1 = 1;
+Q2 = 2;
+Q3 = 3;
+Q4 = 4;
+  
+function bezierCircleForQuadrant(t, quadrant) {  
+    // recall that y is reversed: -y is at the top, +y is at the bottom
+    // bezierCircle: for t 0 -> 1, returns {0,1} to {1, 0}
+    let pt;
+    switch(quadrant) {
+      case Q1:
+        pt = bezierCircle(1 - t);
+        pt.x = -pt.x;
+        pt.y = -pt.y;
+        return pt;
+      case Q2:
+        pt = bezierCircle(t);
+        pt.y = -pt.y;
+        return pt;
+      case Q3:
+        return bezierCircle(1 - t);
+      case Q4: 
+        pt = bezierCircle(t);
+        pt.x = -pt.x;
+        return pt;
+    } 
+  }
+
+iterations = 10000
+t0 = performance.now();
+res = [];
+
+for(let iter = 0; iter < iterations; iter += 1) {
+  for(let q = 1; q < 5; q += 1) {
+    for(let i = 0; i <= 1; i += 0.1) {
+      res.push(bezierCircleForQuadrant(i, q));
+    }
+  }
+}
+t1 = performance.now();
+console.log(`Total: ${(t1 - t0).toPrecision(2)}ms.\nAverage: ${((t1 - t0) / iterations).toPrecision(2)}ms`);
 
 
+bezierCache = new Map();
+t0 = performance.now();
+res2 = [];
+for(let iter = 0; iter < iterations; iter += 1) {
+  for(let q = 1; q < 5; q += 1) {
+    for(let i = 0; i <= 1; i += .1) {
+      tmp = bezierCache.get(i);
+      if(!tmp) {
+        tmp = bezierCircleForQuadrant(i, q);
+        bezierCache.set(q.toString() + i.toString(), tmp);
+      }
+      res2.push(tmp);
+    }
+  }
+}
+t1 = performance.now();
+console.log(`Total: ${(t1 - t0).toPrecision(2)}ms.\nAverage: ${((t1 - t0) / iterations).toPrecision(2)}ms`);
+
+arraysEqual(res, res2);
 
 
 
