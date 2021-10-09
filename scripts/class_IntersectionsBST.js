5fbaa1cbb261976e975a416780f2d59ee6d4e65b
Michael Enion <caewok_2000@yahoo.com>
2021-10-09 12:41:00 -0400

    Add BST classes that sort points by x,y or by y,x

---
+++ scripts/class_IntersectionsBST.js
@@ -0,0 +1,37 @@
+export class IntersectionsXOrderBST extends BinarySearchTree {
+ /**
+  * Compare data in two nodes
+  * For intersections, sort by x, then y. 
+  * Equality is if the intersection is almostEqual to another
+  * @param {Object} a  Node data object
+  * @param {Object} b  Node data object
+  * @return {-1|0|1} 
+  */
+  compare(a, b) {
+    if(almostEqual(a.x, b.x)) {
+      if(almostEqual(a.y, b.y)) { return 0; }
+      return a.y < a.y ? -1 : 1;
+    } else {
+      return a.x < b.x ? -1 : 1; 
+    }
+  }
+}
+
+export class IntersectionsYOrderBST extends BinarySearchTree {
+ /**
+  * Compare data in two nodes
+  * For sweep, sort by y, then x. 
+  * Equality is if the intersection is almostEqual to another
+  * @param {Object} a  Node data object
+  * @param {Object} b  Node data object
+  * @return {-1|0|1} 
+  */
+  compare(a, b) {
+    if(almostEqual(a.y, b.y)) {
+      if(almostEqual(a.x, b.x)) { return 0; }
+      return a.x < a.x ? -1 : 1;
+    } else {
+      return a.y < b.y ? -1 : 1; 
+    }
+  }
+}
