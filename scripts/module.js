84fe31faed7e15e2ea7c98729c17c6a812f881e9
Michael Enion <caewok_2000@yahoo.com>
2021-10-19 08:37:35 -0700

    Add new Pixel/Point/Ray classes to API

--- scripts/module.js
+++ scripts/module.js
@@ -3,15 +3,19 @@
 
 // import { registerCCW } from "./patching.js";
 import { testCCWBenchmarkSight } from "./benchmark.js";
-import { orient2d } from "./lib/orient2d.min.js";
-import { incircle } from "./lib/incircle.min.js";
-import { BinarySearchTree }  from "./class_BinarySearchTree.js";
-import { PotentialWallList } from "./class_PotentialWallList.js";
-import { Bezier } from "./class_Bezier.js";
+import { orient2d }           from "./lib/orient2d.min.js";
+import { incircle }           from "./lib/incircle.min.js";
+import { BinarySearchTree }   from "./class_BinarySearchTree.js";
+import { PotentialWallList }  from "./class_PotentialWallList.js";
+import { Bezier }             from "./class_Bezier.js";
 import { CCWSweepWall }       from "./class_CCWSweepWall.js";
 import { CCWSweepPoint }      from "./class_CCWSweepPoint.js";
 import { CCWSightRay }        from "./class_CCWSightRay.js";
-import { CCWSweepPolygon }        from "./class_CCWSweepPolygon.js";
+import { CCWSweepPolygon }    from "./class_CCWSweepPolygon.js";
+import { CCWPoint }           from "./class_CCWPoint.js";
+import { CCWPixelPoint }      from "./class_CCWPixelPoint.js";
+import { CCWRay }             from "./class_CCWRay.js";
+import { CCWPixelRay }        from "./class_CCWPixelRay.js";
 import { IdentifyIntersections, 
          BruteForceIntersections,
          SimpleSweepIntersections,
@@ -81,6 +85,12 @@ Hooks.once('init', async function() {
     CCWSweepWall: CCWSweepWall,
     CCWSightRay: CCWSightRay,
     CCWSweepPolygon: CCWSweepPolygon,
+    
+    CCWPoint: CCWPoint,
+    CCWPixelPoint: CCWPixelPoint,
+    CCWRay: CCWRay,
+    CCWPixelRay: CCWPixelRay,
+    
     orient2d: orient2d,
     incircle: incircle,
     BinarySearchTree: BinarySearchTree,
