ee87e8be0d46f841fdcfd89b84fb2e5ce35fce95
Michael Enion <caewok_2000@yahoo.com>
2021-10-25 09:35:04 -0700

    Use this.x instead of incorrect reference to p1 and p2 for CCWPoint

--- scripts/class_CCWPoint.js
+++ scripts/class_CCWPoint.js
@@ -126,8 +126,8 @@ export class CCWPoint extends PIXI.Point {
   * @return {boolean}
   */
   almostEqual(p, { EPSILON = PRESET_EPSILON } = {}) {
-    return almostEqual(p1.x, p2.x, { EPSILON }) ? 
-           almostEqual(p1.y, p2.y, { EPSILON }) : false;
+    return almostEqual(this.x, p.x, { EPSILON }) ? 
+           almostEqual(this.y, p.y, { EPSILON }) : false;
   }
   
  /**
