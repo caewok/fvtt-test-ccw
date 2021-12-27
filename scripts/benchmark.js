76824ed5264be0e63514e0eb40106adaf73fbacd
Michael Enion <caewok_2000@yahoo.com>
2021-12-27 08:43:43 -0800

    Add additional benchmarking in different order.
    Avoid caching issues where running repeatedly shows very different results.

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -27,5 +27,16 @@ export async function testCCWBenchmarkSight(n=1000, ...args) {
   await ClockwiseSweepPolygon.benchmark(n, ...args);
   await game.modules.get(MODULE_ID).api.MyClockwiseSweepPolygon.benchmark(n, ...args);
   await game.modules.get(MODULE_ID).api.MyClockwiseSweepPolygon2.benchmark(n, ...args);
-
+  
+  // Run ClockwiseSweep repeatedly and in different orders
+  // To avoid caching and other timing issues
+  console.log(`ClockwiseSweep iteration 2`);
+  await game.modules.get(MODULE_ID).api.MyClockwiseSweepPolygon2.benchmark(n, ...args);
+  await ClockwiseSweepPolygon.benchmark(n, ...args);
+  await game.modules.get(MODULE_ID).api.MyClockwiseSweepPolygon.benchmark(n, ...args);
+  
+  console.log(`ClockwiseSweep iteration 3`);
+  await game.modules.get(MODULE_ID).api.MyClockwiseSweepPolygon2.benchmark(n, ...args);
+  await game.modules.get(MODULE_ID).api.MyClockwiseSweepPolygon.benchmark(n, ...args);
+  await ClockwiseSweepPolygon.benchmark(n, ...args);
 }
