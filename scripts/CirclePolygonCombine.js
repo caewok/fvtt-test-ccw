e067d78951cacfb526d901bcd8feec3f3be8c1c7
Michael Enion <caewok_2000@yahoo.com>
2022-01-04 08:49:41 -0800

    Clean up CirclePolygon
    Add notes.
    Clean up variable names.

--- scripts/CirclePolygonCombine.js
+++ scripts/CirclePolygonCombine.js
@@ -10,13 +10,23 @@ PolygonVertex
 
 //import { log } from "./module.js";
 //import { SimplePolygon, SimplePolygonVertex } from "./SimplePolygon.js";
-import { SimplePolygon2 as SimplePolygon } from "./SimplePolygon2.js";
+import { SimplePolygon } from "./SimplePolygon.js";
 
 
 /*
 Intersect or union a polygon with a circle without immediately converting circle to a polygon. Similar method to that of SimplePolygon._combine. Start at intersection point, tracing polygon or circle. At each intersection point, pick the clockwise (intersect) or counterclockwise (union) direction. Use padding to fill the circle to the next intersection. 
+
+Exported methods are added to PIXI.Circle in PIXICircle.js.
 */
 
+/**
+ * Union of this circle with a polygon.
+ * @param {PIXI.Polygon} poly
+ * Options:
+ * @param {number} density    How many points to use when converting circle arcs to 
+ *                            a polygon.
+ * @return {PIXI.Polygon}
+ */
 export function circle_union(poly, { density = 60 } = {}) {
   // for simplicity, if poly is also a circle, just convert to a polygon
   poly = SimplePolygon.fromPolygon(poly, { density });
@@ -27,13 +37,13 @@ export function circle_union(poly, { density = 60 } = {}) {
   return _combine(poly, this, { clockwise: false, density });
 }
 
- /**
-  * Find the polygon representing the intersection of two polygons.
-  * Polygons must be simple---they cannot intersect themselves.
-  * @param {PIXI.Polygon} poly1
-  * @param {PIXI.Polygon} poly2
-  * @return {PIXI.Polygon}
-  */  
+/**
+ * Intersect of this circle with a polygon.
+ * @param {PIXI.Polygon} poly
+ * @param {number} density    How many points to use when converting circle arcs to 
+ *                            a polygon. 
+ * @return {PIXI.Polygon}
+ */  
 export function circle_intersect(poly, { density = 60 } = {}) {
   poly = SimplePolygon.fromPolygon(poly, { density });
   const out = _combine(poly, this, { clockwise: true, density });
@@ -46,17 +56,18 @@ export function circle_intersect(poly, { density = 60 } = {}) {
   return out;
 }
   
- /**
-  * Combine two polygons. Helper for union and intersect methods.
-  * Polygons must be simple---they cannot intersect themselves.
-  * @param {PIXI.Polygon} poly1
-  * @param {PIXI.Polygon} poly2
-  * Options: 
-  * @param {boolean} clockwise  True if the trace should go clockwise at each 
-  *                             intersection; false to go counterclockwise.
-  * @return {PIXI.Polygon}
-  * @private
-  */
+/**
+ * Helper for union and intersect methods.
+ * @param {PIXI.Polygon} poly
+ * @param {PIXI.Circle}  circle
+ * Options: 
+ * @param {boolean} clockwise  True if the trace should go clockwise at each 
+ *                             intersection; false to go counterclockwise.
+ * @param {number} density    How many points to use when converting circle arcs to 
+ *                            a polygon. 
+ * @return {PIXI.Polygon}
+ * @private
+ */
 function _combine(poly, circle, { clockwise = true, density = 60 } = {}) {
   const pts = _tracePolygon(poly, circle, { clockwise, density });
     
@@ -83,6 +94,14 @@ function _combine(poly, circle, { clockwise = true, density = 60 } = {}) {
   return new_poly; 
 } 
 
+/**
+ * Test whether circle could encompass the polygon
+ * Only certain to encompass if you already know that the two do not intersect.
+ * Equivalent to SimplePolygon.prototype.encompassesPolygon.
+ * @param {PIXI.Circle}   circle
+ * @param {PIXI.Polygon}  poly
+ * @return {boolean}  True if circle could possibly encompass the polygon.
+ */ 
 function _circleEncompassesPolygon(circle, poly) {
   const iter = poly.iteratePoints();
   for(const pt of iter) {
@@ -91,6 +110,24 @@ function _circleEncompassesPolygon(circle, poly) {
   return true;
 }
 
+/**
+ * Trace around a polygon in the clockwise direction. At each intersection with
+ * the second polygon, select either the clockwise or counterclockwise direction 
+ * (based on the option). Return each vertex or intersection point encountered, as
+ * well as padding points for the circle.
+ *
+ * Mark each time the trace jumps from the polygon to the circle, or back. 
+ * When returning to the polygon, add padding points representing the circle arc 
+ * from the starting intersection to this ending intersection.
+ * 
+ * @param {PIXI.Circle}   circle
+ * @param {PIXI.Polygon}  poly
+ * @param {boolean} clockwise   True if the trace should go clockwise at each 
+ *                              intersection; false to go counterclockwise.
+ * @param {number} density      How many points to use when converting circle arcs to 
+ *                              a polygon. 
+ * @return {number[]} Points array, in format [x0, y0, x1, y1, ...]
+ */
 function _tracePolygon(poly, circle, { clockwise = true, density = 60 } = {}) {
   const center = { x: circle.x, y: circle.y };
   const radius = circle.radius;
@@ -99,9 +136,15 @@ function _tracePolygon(poly, circle, { clockwise = true, density = 60 } = {}) {
   // need to locate all the intersections b/c we will draw padding 
   // (polygon version of circle) between each intersection as needed.
   
+/*
+It might be possible to combine this loop with the trace loop below, locating intersections just-in-time. It might also be possible to use some logic to skip edges that are definitely outside or definitely inside the circle. Not sure that would be faster, but combining both might be, because the trace loop already tracks whether we are current inside or outside the circle (indirectly, by choosing clockwise/ccw directions).
+*/
+  
   let first_edge = undefined;
   for(const edge of poly.edges) {
     // locate first intersection
+    // lineCircleIntersection already skips some tests if the edges are both inside
+    // the circle, so probably no benefit to further testing that here.
     const x = foundry.utils.lineCircleIntersection(edge.A, edge.B, center, radius);
     if(x.intersections.length > 0) {
       if(!first_edge) first_edge = edge;
@@ -111,7 +154,6 @@ function _tracePolygon(poly, circle, { clockwise = true, density = 60 } = {}) {
   
   // if no intersections, can return.
   if(!first_edge) { return pts; }
-  
   
   let curr_edge = first_edge;
   let tracing_segment = true;
@@ -134,7 +176,7 @@ function _tracePolygon(poly, circle, { clockwise = true, density = 60 } = {}) {
                                                                density});
       tracing_segment = res0.tracing_segment;
       circle_start = res0.circle_start;
-      if(res0.pts.length > 0) { pts.push(...res0.pts); }
+      if(res0.padding.length > 0) { pts.push(...res0.padding); }
       
       // we started at the first intersection, so stop once we get back to it
       if(curr_edge === first_edge && i > 0) break; 
@@ -147,15 +189,10 @@ function _tracePolygon(poly, circle, { clockwise = true, density = 60 } = {}) {
                                                                  density});
         tracing_segment = res1.tracing_segment;
         circle_start = res1.circle_start;
-        if(res1.pts.length > 0) { pts.push(...res1.pts); }
+        if(res1.padding.length > 0) { pts.push(...res1.padding); }
       }
     } // done with intersection processing
-    
-    // shouldn't ever hit this test, b/c we always return to an intersection
-    // break in the intersection processing test instead.
-    // if(curr_edge === first_edge && i > 0) break;
-    
-    
+      
     if(tracing_segment) {
       // add the edge B vertex to points array unless we already did 
       // (if it was an intersection, would have been added above)
@@ -175,18 +212,24 @@ function _tracePolygon(poly, circle, { clockwise = true, density = 60 } = {}) {
  * Helper to process a single intersection. Used b/c it is possible for a circle to have 
  * multiple intersections. 
  * @param {LineCircleIntersection} circle_x
- * @param {number} i  Which intersection to process
- * @param {Point} center  Center of the circle
+ * @param {number}  i               Which intersection to process
+ * @param {Point}   center          Center of the circle
  * Options:
- * @param {boolean} clockwise   True if the clockwise/intersect direction should be used.
+ * @param {boolean} clockwise       True if the clockwise/intersect direction 
+ *                                  should be used.
  * @param {boolean} tracing_segment Are we currently on the segment?
- * @param {Point} circle_start  Prior intersection used to connect circle
- * @param {number} density      How much padding (polygon points) to use?
+ * @param {Point}   circle_start    Prior intersection used to connect circle
+ * @param {number}  density         How much padding (polygon points) to use?
+ * @return {Object} The padding points, whether we are tracing the segment, and the 
+ *                  starting intersection for the circle, if any.
+ *                  { padding: Array[number],
+ *                    tracing_segment: {boolean },
+ *                    circle_start: {Point} }
  */
 function _processIntersection(circle_x, x_i, center, { clockwise = true, 
-                                            tracing_segment = true, 
-                                            circle_start = undefined,
-                                            density = 60 } = {}) {
+                                                       tracing_segment = true, 
+                                                       circle_start = undefined,
+                                                       density = 60 } = {}) {
   const x = circle_x.intersections[x_i];
   if(!x) return;
   
@@ -218,12 +261,10 @@ have an intersection
   // two intersections
   } else if(circle_x.intersections.length === 2) {
     // we must have a outside --> i0 ---> i1 ---> b outside
-    if(x_i) { // second intersection
-      tracing_segment = !clockwise
-    } else { // first intersection
-      tracing_segment = clockwise
-    }
-    
+    // if x_i is 1, we are on second intersection
+    // if x_i is 0, we are on first intersection
+    tracing_segment = x_i ? !clockwise : clockwise; 
+        
   // one intersection  
   } else if(circle_x.bInside) { 
     tracing_segment = clockwise; // on circle if we want CCW direction
@@ -251,14 +292,24 @@ have an intersection
   // (skip if we are just continuing the circle)
   if(was_tracing_segment || tracing_segment) { padding.push(x.x, x.y); }
   
-  return { pts: padding,
+  return { padding: padding,
            tracing_segment: tracing_segment,
            circle_start: circle_start };
 }
 
-function paddingPoints(fromPt, toPoint, center, { density = 60 } = {}) {
+/**
+ * "Pad" by adding points representing a circle arc between fromPoint and toPoint.
+ * Relies on ClockwiseSweepPolygon.prototype._getPaddingPoints.
+ * @param {Point} fromPt
+ * @param {Point} toPoint
+ * @param {Point} center    Center of the circle
+ * Options:
+ * @param {number} density          How much padding (polygon points) to use?
+ * @return {number[]} Points array, in format [x0, y0, x1, y1, ...]
+ */
+function paddingPoints(fromPoint, toPoint, center, { density = 60 } = {}) {
   const obj = { config: { density }};
-  const r0 = new Ray(center, fromPt);
+  const r0 = new Ray(center, fromPoint);
   const r1 = new Ray(center, toPoint);
   
   return ClockwiseSweepPolygon.prototype._getPaddingPoints.call(obj, r0, r1);
