ddf51cafac8a85e9ebbc33b18e48bcc4db2e8ea4
Michael Enion <caewok_2000@yahoo.com>
2022-01-09 13:16:26 -0800

    Add ActiveEdges to API

--- scripts/module.js
+++ scripts/module.js
@@ -13,6 +13,8 @@ import { registerPIXIPolygonMethods } from "./PIXIPolygon.js";
 import { registerPIXIRectangleMethods } from "./PIXIRectangle.js";
 import { registerPIXICircleMethods } from "./PIXICircle.js";
 
+import { ActiveEdges } from "./ActiveEdgesIterator.js";
+
 import * as ClipperLib from "./lib/clipper_unminified.js";
  
 export const MODULE_ID = 'testccw';
@@ -75,6 +77,8 @@ Hooks.once('init', async function() {
     SimplePolygon: SimplePolygon,
     SimplePolygonVertex: PolygonVertex,
     SimplePolygonEdge: SimplePolygonEdge,
+    
+    ActiveEdges: ActiveEdges
     
     }
 });
