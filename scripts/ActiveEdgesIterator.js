0309d61daf2f7a40ffac907d49178d0bda86122c
Michael Enion <caewok_2000@yahoo.com>
2022-01-09 07:38:03 -0800

    Fixes to ActiveEdges class
    Appears to have basic functionality.
    Skipping based on previous end index not yet working.
    Changed to find the union of the aEdge and bEdges, which should be faster then getting the intersection of the larger two sets.

--- scripts/ActiveEdgesIterator.js
+++ scripts/ActiveEdgesIterator.js
@@ -4,10 +4,10 @@ foundry
 'use strict';
 
 // Proposed rules:
-// - a_start_index stays the same or increases.
-// - b_start_index either stays the same or increase.
-// - a_start_index begins at 0. 
-// - b_start_index begins at 0.
+// √ a_start_index stays the same or increases.
+// √ b_start_index either stays the same or increase.
+// √ a_start_index begins at 0. 
+// √ b_start_index begins at 0.
 // - a_end_index stays the same or increase, circling back to 0+.
 // - b_end_index stays the same or increase, circling back to 0+.
 
@@ -16,7 +16,6 @@ export class ActiveEdges {
     this.vertices = sorted_vertices;
     this.origin = origin;
     this.starting_idx = starting_idx;    
-    this.start_indices = { A: undefined, B: undefined };
     this.end_indices = { A: undefined, B: undefined };
     this.edges = this._constructEdgeArrays();
   }
@@ -51,9 +50,7 @@ export class ActiveEdges {
   * Get the iterator object containing the values for each activeEdge set
   * from starting_idx to the length of the vertices.
   */
-  values() {
-    return this[Symbol.iterator]();
-  } 
+  values() { return this[Symbol.iterator](); } 
   
  /**
   * Create aEdges and bEdges arrays. 
@@ -82,10 +79,7 @@ export class ActiveEdges {
  /**
   * Reset the stored indices for the class object.
   */
-  reset() { 
-    this.end_indices = { A: undefined, B: undefined }; 
-    this.start_indices = { A: undefined, B: undefined };
-  } 
+  reset() { this.end_indices = { A: undefined, B: undefined }; } 
   
 /**
  * Helper functions to count from a start number in sequence 0 to n, 
@@ -123,100 +117,129 @@ export class ActiveEdges {
   }
     
  /**
-  * Given a vertex index, get the set of possible active edges from the 
+  * Given a vertex index, get the set of active edges from the 
   * aEdges or bEdges queue. Search the aEdge queue in reverse; bEdge queue forward 
-  * Any values between start index and the stored end index are automatically added.
+  * aEdges: From the current vertex or nearest prior A vertex, move backward.
+  *         Stop when an edge's B vertex is CCW. All intervening in active edge set.
+  * bEdges: From the current vertex or nearest next B vertex, move forward.
+  *         Stop when an edge's A vertex is CW. All intervening in active edge set.
+  *
+  * As we progress forward in the vertex queue, the aEdge end index and bEdge end index
+  * will both move forward. Thus, we can simply search forward from the last end index
+  * if we have one available. Need to be careful about wrap-around, however.
   */
   activeEdges(vertex_idx, start_idx, type = "A") {
-    const vertex = this.vertices[vertex_idx];
     const end_idx = this.end_indices[type];
-    const origin = this.origin;
+    
+    //if(typeof end_idx === "undefined") 
+      return this._activeEdgesNewSearch(vertex_idx, start_idx, type);
+  
+    // with an end index, 
+    // we can move forward until we pass the test, 
+    // then add all edges between start index and end index.
+    const test     = type === "A" ? this.aEdgeTest : this.bEdgeTest;
+    const count_fn = this.circleForward;
     const edges = this.edges[type];
+    const vertex = this.vertices[vertex_idx];
+        
+    const ln = edges.length;
+    for(let i = 0; i < ln; i += 1) {
+      const idx = count_fn(i, end_idx, ln);
+      const edge = edges[idx];
+      
+      if(test.call(this, vertex, edge)) {
+        this.end_indices[type] = idx;
+        break;
+      }
+    }
     
-    // add edges from start_idx to the past end index, not including the end index
-    const active_edges = this._startingActiveEdgeSet(start_idx, end_idx, edges);
-    start_idx = end_idx ?? start_idx;
+    if(type === "A") {
+      // get between end and start
+      // inclusive of end index and start index
+      return new Set(this.wrappedSlice(edges, this.end_indices[type], start_idx + 1));
+    } else {
+      // get between start and end
+      // inclusive of end index and start index
+      return new Set(this.wrappedSlice(edges, start_idx, this.end_indices[type] + 1));
+    }  
+  }    
     
+  /**
+   * Return a wrapped slice of array items.
+   * If end is less than start, return start:ln and then 0:end.
+   */
+   wrappedSlice(arr, start, end) {
+     const ln = arr.length;
+     
+     if(start === end) return [];
+     
+     // these checks may not be necessary for this class...
+     if(end >= ln) end = end % ln;
+     if(start >= ln) start = start % ln;
+     
+     if((typeof start === "undefined") || (typeof end === "undefined")) { 
+       return arr.slice(start, end);
+     }
+     
+     // no wrap necessary, so return a regular slice
+     if(start < end) {
+       return arr.slice(start, end);
+     }
+     
+     // end is less than start, so we need to wrap
+     return arr.slice(start, ln).concat(arr.slice(0, end));
+   }
+  
+  /**
+   * Test for whether an aEdge should be in active set.
+   * We want to stop (return false) when we hit a ccw B vertex
+   */
+   aEdgeTest(vertex, edge) {
+     const origin = this.origin;
+     // orient2d returns positive for ccw
+     return foundry.utils.orient2dFast(origin, vertex, edge.B) < 0;
+   }
+
+  /**
+   * Test for whether an bEdge should be in active set.
+   * We want to stop (return false) when we hit a cw A vertex
+   */
+   bEdgeTest(vertex, edge) {
+     const origin = this.origin;
+     return foundry.utils.orient2dFast(origin, vertex, edge.A) > 0;
+   }
+  
+  /**
+   * No end index available, so search from start either forward or back, adding to 
+   * active set as we go.
+   */
+  _activeEdgesNewSearch(vertex_idx, start_idx, type = "A") {
     const countfn = type === "A" ? this.circleBackward : this.circleForward; 
-    const test = type === "A" ? (orient => orient < 0) : (orient => orient > 0);
-    
-    this.start_indices[type] = start_idx;
+    const test = type === "A" ? this.aEdgeTest : this.bEdgeTest;
+    const edges = this.edges[type];
+    const vertex = this.vertices[vertex_idx];
+    const active_edges = new Set();
+        
     const ln = edges.length;
     for(let i = 0; i < ln; i += 1) {
       const idx = countfn(i, start_idx, ln);
-    
-      // orient2d returns positive for ccw
-      // if A is ccw to o|v, A might be in the set of activeEdges
-      // if B is cw to o|v, B might be in the set of activeEdges
-      if(test(foundry.utils.orient2dFast(origin, vertex, edges[idx][type]))) {
+      const edge = edges[idx];
+      
+      if(!test.call(this, vertex, edge)) {
+        // we have reached the end of active edges in this direction
+        // mark the immediately prior edge as the last true
+//         this.end_indices[type] = type === "A" ? (idx + 1) % ln : idx - 1;
+//         if(this.end_indices[type] < 0) { this.end_indices[type] += ln; }
         this.end_indices[type] = idx;
         return active_edges;
       }
-  
-      // add to set of possible active edges
-      //console.log(`Adding ${edgeLabel(aEdges[idx])} to activeEdgesA`);
-      active_edges.add(edges[idx]);
-    }
-    return active_edges;
-  }
-  
- /**
-  * Construct the start set of active edges. 
-  * Set includes edges between start index and end index, including the start but not the
-  * end. Circles around if end is less than start. 
-  * @param {number}           start_idx   Starting edge to use.
-  * @param {number|undefined} end_idx     End edge to use or undefined for the blank set.
-  * @param {PolygonEdges[]}   edges       Array of edges to use in constructing the set.
-  * @return {Set<PolygonEdges>} Set of polygon edges, possible a null set.
-  */
-  _startingActiveEdgeSet(new_start_idx, edges, type = "A") {  
-    let prior_end_idx = this.end_indices[type];
-    const prior_start_idx = this.start_indices[type];
-    const ln = this.vertices.length;
-    
-    if( (typeof end_idx === "undefined") ||  
-        (typeof start_idx === "undefined")) {
-      return { active_edges: new Set(), start_idx: new_start_idx };
-    }
-    
-    if(type === "B") {
-      // B vertices: circling forward.
-      // For B type, the end index could wrap; revert to linear by adding ln if 
-      // the end is less than the prior start
-      if(prior_end_idx < prior_start_idx) { prior_end_idx += ln; }
-    
-      // assume for now it is possible for the start index to wrap as well
-      if(new_start_idx < prior_start_idx) { new_start_idx += ln; }
       
-    
-      // now that everything is linear, if new start has passed the prior end, 
-      // we want a null set
-      if(new_start_idx >= prior_end_idx) {
-        return { active_edges: new Set(), start_idx: new_start_idx % ln };
-      }
-       
-      // otherwise, we want to add the intervening elements, which is a bit tricky 
-      // b/c we might go past the end of the edge array
-      if(prior_end_idx >= ln) {
-        // we are past the end of the edge array
-        const s = new Set(edges.slice(new_start_idx % ln));
-        edges.slice(0, prior_end_idx % ln).forEach(e => s.add(e));
-        return { active_edges: s, start_idx: new_start_idx % ln };
-      } 
+      // add to set of possible active edges
+      active_edges.add(edge);
     }
-    
-    // type "A" vertices: circling backward
-    if(prior_end_idx > prior_start_idx) { prior_end_idx += ln; }
-    if(new_start_idx > prior_start_idx) { new_start_idx += ln; }
-    
-    // now that everything is linear, if new start is less than prior end,
-    // we want a null set
-    if(new_start_idx)
-    
+    return active_edges; // should probably never hit this
   }
-
-   
-  
+    
  /**
   * Get the active edges given the index of the current vertex in the vertex queue. 
   */
@@ -227,12 +250,41 @@ export class ActiveEdges {
     const activeEdgesA = this.activeEdges(vertex_idx, a_start_idx, "A");
     const activeEdgesB = this.activeEdges(vertex_idx, b_start_idx, "B");
     
-    // active edges are the intersection of the A and B sets.
+    // active edges are the union of the A and B sets.
     // but subtract out the cwEdges of the vertex, which are technically not yet
     // part of the active edge set
     
-    const activeEdges = activeEdgesA.intersection(activeEdgesB);
+    const activeEdges = activeEdgesA.union(activeEdgesB);
     return activeEdges.diff(this.vertices[vertex_idx].cwEdges);
+  }
+  
+  // -------------- DEBUGGING ----------------------------- //
+  
+ /**
+  * Provide a label for the edge corresponding to ClockwiseSweep visualization,
+  * whereby the first vertex is labeled 1. Edge with vertex 0 for A and vertex 1 
+  * for B would therefore be labeled 1|2. 
+  * @param {PolygonEdge} edge   Edge to label.
+  * @return {string} Label in form of A|B.
+  */ 
+  edgeLabel(edge) {
+    const a = this.vertices.findIndex(v => v.key === edge.A.key) + 1;
+    const b = this.vertices.findIndex(v => v.key === edge.B.key) + 1;
+    return `${a}|${b}`;
+  }
+
+ /**
+  * Provide labels for an array of edges, using the edgeLabel method for each.
+  * Print the array labels to console with provided label, if any.
+  * @param {PolygonEdge[]}  edgeArray   Array of edges to label.
+  * @param {string}         label       Prefix label to use in the console output.
+  */
+  listEdges(edgeArray, label = "") {
+    const edge_str = [];
+    for(const edge of edgeArray) {
+      edge_str.push(this.edgeLabel(edge));
+    }  
+    console.log(`${label}${edge_str.join(', ')}`);
   }
 }
 
@@ -240,16 +292,6 @@ Set.prototype.diff = function(other) {
   return new Set([...this].filter(x => !other.has(x)));
 }
 
-edgeLabel = function(edge) {
-  const a = vertices.findIndex(v => v.key === edge.A.key) + 1;
-  const b = vertices.findIndex(v => v.key === edge.B.key) + 1;
-  return `${a}|${b}`;
-}
-
-listEdges = function(edgeArray, label = "") {
-  edge_str = [];
-  for(edge of edgeArray) {
-    edge_str.push(edgeLabel(edge));
-  }  
-  console.log(`${label}${edge_str.join(', ')}`);
+Set.prototype.union = function(other) {
+  return new Set([...this, ...other]);
 }
