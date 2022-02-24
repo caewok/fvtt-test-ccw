8e85e532a7172e0536b0c1a7984be83178751c3d
Michael Enion <caewok_2000@yahoo.com>
2022-02-24 03:46:53 -0800

    Added intersection tests
    Whether two segments intersect and the intersection point of two lines. The latter uses a robust method, saving the division step to last.

--- geometry/src/lib.rs
+++ geometry/src/lib.rs
@@ -34,10 +34,36 @@ pub fn orient2d(a: &Point, b: &Point, c: &Point) -> f64 {
 ///
 /// ## Returns
 /// True if the lines segments intersect.
-pub fn lineSegmentIntersects(a: &Point, b: &Point, c: &Point, d: &Point) -> bool {
+pub fn line_segment_intersects(a: &Point, b: &Point, c: &Point, d: &Point) -> bool {
   let xab = (orient2d(a, b, c) * orient2d(a, b, d)) <= 0.0;
   let xcd = (orient2d(c, d, a) * orient2d(c, d, b)) <= 0.0;
   return xab && xcd
+}
+
+/// Compute the intersection between two infinite lines.
+/// Does not check for parallel lines; will return Infinite, NaN coordinates in that case
+///
+/// ## Arguments
+/// *a* First endpoint of AB
+/// *b* Second endpoint of AB
+/// *c* First endpoint of CD
+/// *d* Second endpoint of CD
+///
+/// ## Returns
+/// Coordinates of the intersection.
+pub fn line_line_intersection(a: &Point, b: &Point, c: &Point, d: &Point) -> Point {
+	let dx1 = b.x - a.x;
+  	let dx2 = d.x - c.x;
+  	let dy1 = b.y - a.y;
+  	let dy2 = d.y - c.y;
+
+  	let x_num = a.x * dy1 * dx2 - c.x * dy2 * dx1 + c.y * dx1 * dx2 - a.y * dx1 * dx2;
+  	let y_num = a.y * dx1 * dy2 - c.y * dx2 * dy1 + c.x * dy1 * dy2 - a.x * dy1 * dy2;
+
+  	let x_dnm = dy1 * dx2 - dy2 * dx1;
+  	let y_dnm = dx1 * dy2 - dx2 * dy1;
+
+  	Point { x: x_num / x_dnm, y: y_num / y_dnm }
 }
 
 #[cfg(test)]
@@ -49,7 +75,7 @@ mod tests {
 	// d is to right of a|b
 	// e is collinear with a|b
 	// c|d intersects a|b
-	// d|f parallel to a|b
+	// d|f parallel to a|b, c|d does not intersect as a segment but does as infinite line
 
     #[test]
     fn ccw_orientation_works() {
@@ -135,7 +161,7 @@ mod tests {
 			y: 2100.0,
 		};
 
-		let is_ix = lineSegmentIntersects(&a, &b, &c, &d);
+		let is_ix = line_segment_intersects(&a, &b, &c, &d);
 		assert!(is_ix);
 	}
 
@@ -161,7 +187,34 @@ mod tests {
 			y: 2100.0,
 		};
 
-		let is_ix = lineSegmentIntersects(&a, &b, &d, &f);
+		let is_ix = line_segment_intersects(&a, &b, &d, &f);
 		assert!(!is_ix);
+	}
+
+	#[test]
+	fn intersection() {
+		let a = Point {
+			x: 2300.0,
+			y: 1900.0,
+		};
+
+		let b = Point {
+			x: 4200.0,
+			y: 1900.0,
+		};
+
+		let c = Point {
+			x: 2387.0,
+			y: 1350.0,
+		};
+
+		let e = Point {
+			x: 3200.0,
+			y: 1900.0,
+		};
+
+		let ix = line_line_intersection(&a, &b, &c, &e);
+		assert_eq!(ix.x, 3200.0);
+		assert_eq!(ix.y, 1900.0);
 	}
 }
