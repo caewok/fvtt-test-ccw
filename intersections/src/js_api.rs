21fc6201fa318441edf80f0bdd8fb000cba1c9f6
Michael Enion <caewok_2000@yahoo.com>
2022-03-11 10:07:30 -0800

    Try using 2D array to store intersection results
    Fill with None.
    Then change the elements where there is an intersection.

---
+++ intersections/src/js_api.rs
@@ -0,0 +1,7 @@
+// Interface to Javascript
+// For speed, alloc memory in WASM and use pointers in Javascript
+// Pass in flat arrays of coordinates for segments
+
+// https://radu-matei.com/blog/practical-guide-to-wasm-memory/
+// https://github.com/WebAssembly/design/issues/1231
+
