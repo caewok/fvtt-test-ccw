723023305ab302bc4bf3e2e4035ce93f3a3d7136
Michael Enion <caewok_2000@yahoo.com>
2022-03-13 17:28:10 -0700

    Switch to separate trait impl for i32 vs f64
    Cannot easily deal with int overflow with generic <T> implementations. Instead, handle each separately, by implementing for specific Coordinate<T>.

--- intersections/src/main.rs
+++ intersections/src/main.rs
@@ -3,28 +3,29 @@
 // pub mod intersections;
 // pub mod js_api;
 
-use intersections::js_api::brute_i32;
+// use intersections::js_api::brute_i32;
 
 fn main() {
-	let coordinates: Vec<i32> = vec![
-			2300, 1900, 4200, 1900,
-			2387, 1350, 2500, 2100,
-			2387, 1350, 3200, 1900,
-			2500, 2100, 2900, 2100,
-		];
-
-
-	let expected: Vec<f64> = vec![
-		2469.866666666667, 1900., 0., 1.,
-		3200., 1900., 0., 2.,
-		2387., 1350., 1., 2.,
-		2500., 2100., 1., 3.,
-	];
-
-	dbg!(&coordinates);
-	dbg!(&expected);
-
-	let res = brute_i32(&coordinates[..]);
-	dbg!(&res);
+	println!("Hello world!");
+// 	let coordinates: Vec<i32> = vec![
+// 			2300, 1900, 4200, 1900,
+// 			2387, 1350, 2500, 2100,
+// 			2387, 1350, 3200, 1900,
+// 			2500, 2100, 2900, 2100,
+// 		];
+//
+//
+// 	let expected: Vec<f64> = vec![
+// 		2469.866666666667, 1900., 0., 1.,
+// 		3200., 1900., 0., 2.,
+// 		2387., 1350., 1., 2.,
+// 		2500., 2100., 1., 3.,
+// 	];
+//
+// 	dbg!(&coordinates);
+// 	dbg!(&expected);
+//
+// 	let res = brute_i32(&coordinates[..]);
+// 	dbg!(&res);
 
 }
--- intersections/src/point.rs
+++ intersections/src/point.rs
@@ -1,8 +1,5 @@
 use geo::{Point, CoordNum, Coordinate};
 use geo::algorithm::kernels::Orientation;
-use num_traits::{ Signed };
-// use std::ops::Neg;
-// use std::num::Saturating;
 use rand::prelude::Distribution;
 use rand::distributions::Standard;
 use rand::distributions::uniform::SampleUniform;
@@ -42,48 +39,54 @@ impl<T> GenerateRandom for Point<T>
 	}
 }
 
-pub fn orient2d<T>(a: Coordinate<T>, b: Coordinate<T>, c: Coordinate<T>) -> Orientation
-	where T: CoordNum + Signed,
-{
-	let dac = a - c;
-	let dbc = b - c;
+/*
+Using T for different types is possible, but it is very difficult to handle integer overflow.
+If passed i32, it is possible to overflow with the subtraction or multiplication.
+(More likely when calculating intersections, but..)
+Cannot just use check overflow functions b/c they are not implemented for floats.
 
-	let res = dac.y * dbc.x - dac.x * dbc.y;
-	let z:T = num_traits::zero();
+Instead, implement an orientation trait and switch on different coordinate types.
+https://stackoverflow.com/questions/56100579/how-do-i-match-on-the-concrete-type-of-a-generic-parameter
+*/
+pub trait SimpleOrient<B = Self, C = Self> {
+	fn orient2d(self, b: B, c: C) -> Orientation;
+}
 
-	if res > z {
-		Orientation::CounterClockwise
-	} else if res < z {
-		Orientation::Clockwise
-	} else {
-		Orientation::Collinear
+impl SimpleOrient for Coordinate<f64> {
+	fn orient2d(self, b: Self, c: Self) -> Orientation {
+		let dac = self - c;
+		let dbc = b - c;
+     	let res = dac.y * dbc.x - dac.x * dbc.y;
+     	if res > 0. {
+     		Orientation::CounterClockwise
+     	} else if res < 0. {
+     		Orientation::Clockwise
+     	} else {
+     		Orientation::Collinear
+     	}
 	}
 }
 
-pub fn orient2drobust<T>(a: Coordinate<T>, b: Coordinate<T>, c: Coordinate<T>) -> Orientation
-	where T: CoordNum + Signed, f64: From<T>,
-{
-	let orientation = robust::orient2d(
-		robust::Coord {
-			x: a.x,
-			y: a.y,
-		},
-		robust::Coord {
-			x: b.x,
-			y: b.y,
-		},
-		robust::Coord {
-			x: c.x,
-			y: c.y,
-		},
-	);
-	// robust orientation flipped b/c y-axis is flipped
-	if orientation > 0. {
-		Orientation::Clockwise
-	} else if orientation < 0. {
-		Orientation::CounterClockwise
-	} else {
-		Orientation::Collinear
+impl SimpleOrient for Coordinate<i32> {
+	fn orient2d(self, b: Self, c: Self) -> Orientation {
+		// our choices are try w/o conversion using overflow checks or
+		// convert upfront to i64.
+		let (ax, ay) = self.x_y().into();
+		let (bx, by) = b.x_y().into();
+		let (cx, cy) = c.x_y().into();
+
+		let (ax, ay) = (ax as i64, ay as i64);
+		let (bx, by) = (bx as i64, by as i64);
+		let (cx, cy) = (cx as i64, cy as i64);
+
+		let res = (ay - cy) * (bx - cx) - (ax - cx) * (by - cy);
+		if res > 0 {
+			Orientation::CounterClockwise
+		} else if res < 0 {
+			Orientation::Clockwise
+		} else {
+			Orientation::Collinear
+		}
 	}
 }
 
@@ -94,55 +97,43 @@ mod tests {
 
 // ---------------- ORIENTATION
 	#[test]
-	fn orient_point_int_works() {
-		let p1: Point<i64> = Point::new(0, 0);
-		let p2: Point<i64> = Point::new(1, 1);
-		let p3: Point<i64> = Point::new(0, 1); // cw
-		let p4: Point<i64> = Point::new(1, 0); // ccw
-		let p5: Point<i64> = Point::new(2, 2); // collinear
+	fn orient_point_int32_works() {
+		let p1: Coordinate<i32> = Coordinate { x:0, y:0 };
+		let p2: Coordinate<i32> = Coordinate { x:1, y:1 };
+		let p3: Coordinate<i32> = Coordinate { x:0, y:1 }; // cw
+		let p4: Coordinate<i32> = Coordinate { x:1, y:0 }; // ccw
+		let p5: Coordinate<i32> = Coordinate { x:2, y:2 }; // collinear
 
-		assert_eq!(orient2d(p1.into(), p2.into(), p3.into()), Orientation::Clockwise);
-		assert_eq!(orient2d(p1.into(), p2.into(), p4.into()), Orientation::CounterClockwise);
-		assert_eq!(orient2d(p1.into(), p2.into(), p5.into()), Orientation::Collinear);
+		assert_eq!(p1.orient2d(p2, p3), Orientation::Clockwise);
+		assert_eq!(p1.orient2d(p2, p4), Orientation::CounterClockwise);
+		assert_eq!(p1.orient2d(p2, p5), Orientation::Collinear);
 	}
 
 	#[test]
-	fn orient_point_float_works() {
-		let p1: Point<f64> = Point::new(0., 0.);
-		let p2: Point<f64> = Point::new(1., 1.);
-		let p3: Point<f64> = Point::new(0., 1.); // cw
-		let p4: Point<f64> = Point::new(1., 0.); // ccw
-		let p5: Point<f64> = Point::new(2., 2.); // collinear
+	fn orient_point_float64_works() {
+		let p1: Coordinate<f64> = Coordinate { x:0., y:0. };
+		let p2: Coordinate<f64> = Coordinate { x:1., y:1. };
+		let p3: Coordinate<f64> = Coordinate { x:0., y:1. }; // cw
+		let p4: Coordinate<f64> = Coordinate { x:1., y:0. }; // ccw
+		let p5: Coordinate<f64> = Coordinate { x:2., y:2. }; // collinear
 
-		assert_eq!(orient2d(p1.into(), p2.into(), p3.into()), Orientation::Clockwise);
-		assert_eq!(orient2d(p1.into(), p2.into(), p4.into()), Orientation::CounterClockwise);
-		assert_eq!(orient2d(p1.into(), p2.into(), p5.into()), Orientation::Collinear);
+		assert_eq!(p1.orient2d(p2, p3), Orientation::Clockwise);
+		assert_eq!(p1.orient2d(p2, p4), Orientation::CounterClockwise);
+		assert_eq!(p1.orient2d(p2, p5), Orientation::Collinear);
 	}
 
 	#[test]
-	fn orientrobust_point_int_works() {
-		let p1: Point<i32> = Point::new(0, 0);
-		let p2: Point<i32> = Point::new(1, 1);
-		let p3: Point<i32> = Point::new(0, 1); // cw
-		let p4: Point<i32> = Point::new(1, 0); // ccw
-		let p5: Point<i32> = Point::new(2, 2); // collinear
+	fn orient_point_int32_overflow_works() {
+		let p1: Coordinate<i32> = Coordinate { x:i32::MAX - 2, y: i32::MAX - 2 };
+		let p2: Coordinate<i32> = Coordinate { x:i32::MAX - 1, y: i32::MAX - 1 };
+		let p3: Coordinate<i32> = Coordinate { x:i32::MAX - 2, y: i32::MAX - 1 }; // cw
+		let p4: Coordinate<i32> = Coordinate { x:i32::MAX - 1, y: i32::MAX - 2 }; // ccw
+		let p5: Coordinate<i32> = Coordinate { x:i32::MAX, y:i32::MAX }; // collinear
 
-		assert_eq!(orient2drobust(p1.into(), p2.into(), p3.into()), Orientation::Clockwise);
-		assert_eq!(orient2drobust(p1.into(), p2.into(), p4.into()), Orientation::CounterClockwise);
-		assert_eq!(orient2drobust(p1.into(), p2.into(), p5.into()), Orientation::Collinear);
-	}
+		assert_eq!(p1.orient2d(p2, p3), Orientation::Clockwise);
+		assert_eq!(p1.orient2d(p2, p4), Orientation::CounterClockwise);
+		assert_eq!(p1.orient2d(p2, p5), Orientation::Collinear);
 
-	#[test]
-	fn orientrobust_point_float_works() {
-		let p1: Point<f64> = Point::new(0., 0.);
-		let p2: Point<f64> = Point::new(1., 1.);
-		let p3: Point<f64> = Point::new(0., 1.); // cw
-		let p4: Point<f64> = Point::new(1., 0.); // ccw
-		let p5: Point<f64> = Point::new(2., 2.); // collinear
-
-		assert_eq!(orient2drobust(p1.into(), p2.into(), p3.into()), Orientation::Clockwise);
-		assert_eq!(orient2drobust(p1.into(), p2.into(), p4.into()), Orientation::CounterClockwise);
-		assert_eq!(orient2drobust(p1.into(), p2.into(), p5.into()), Orientation::Collinear);
 	}
 }
 
