fc1f18da4b16c18d468f82ec391a771ec9a8affc
Michael Enion <caewok_2000@yahoo.com>
2022-04-14 07:13:22 -0700

    Add compareXY, YX with almostEqual tests

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -5,7 +5,7 @@ foundry,
 
 'use strict';
 
-/* Utility functions 
+/* Utility functions
 Three functions build on one another to test whether a point is within a pixel of being
 on a line. Used in MyClockwiseSweep to test if a point is on a limited angle ray.
 - equivalentPixel: Could two coordinates be the same pixel?
@@ -13,14 +13,14 @@ on a line. Used in MyClockwiseSweep to test if a point is on a limited angle ray
 - pixelLineContainsPoint: Does a coordinate lie nearly (within a pixel) of a line?
 
 Sort two points for locating line-line intersections:
-- compareXY: Sort points nw to se. 
+- compareXY: Sort points nw to se.
 */
 
 /**
  * Measure whether two coordinates could be the same pixel.
  * Points within √2 / 2 distance of one another will be considered equal.
- * Consider coordinates on a square grid: √2 / 2 is the distance from any 
- * corner of the square to the center. Thus, any coordinate within the square that 
+ * Consider coordinates on a square grid: √2 / 2 is the distance from any
+ * corner of the square to the center. Thus, any coordinate within the square that
  * is within √2 / 2 of a corner can be "claimed" by the pixel at that corner.
  * @param {Point} p1
  * @param {Point} p2
@@ -32,10 +32,10 @@ function equivalentPixel(p1, p2) {
   // if points exactly vertical or horizontal, the x/y would need to be within √2 / 2
   const dx = Math.abs(p2.x - p1.x);
   if(dx > Math.SQRT1_2) return false; // Math.SQRT1_2 === √2 / 2
-  
+
   const dy = Math.abs(p2.y - p1.y);
   if(dy > Math.SQRT1_2) return false;
-  
+
   // within the √2 / 2 bounding box
   // compare distance squared.
   const dist2 = Math.pow(dx, 2) + Math.pow(dy, 2);
@@ -48,17 +48,17 @@ function equivalentPixel(p1, p2) {
  * See equivalentPixel function for further discussion on the choice of √2 / 2.
  * @param {Ray} ray
  * @param {Point} c
- * @return {number}   Same as foundry.utils.orient2dFast 
- *                    except 0 if within √2 /2 of the ray. 
+ * @return {number}   Same as foundry.utils.orient2dFast
+ *                    except 0 if within √2 /2 of the ray.
  *                    Positive: c counterclockwise/left of A|B
  *                    Negative: c clockwise/right of A|B
  *                    Zero: A|B|C collinear.
  */
 function orient2dPixelLine(ray, c) {
-  const orientation = foundry.utils.orient2dFast(ray.A, ray.B, c);  
+  const orientation = foundry.utils.orient2dFast(ray.A, ray.B, c);
   const orientation2 = orientation * orientation;
-  const cutoff = 0.5 * Math.pow(ray.distance, 2); // 0.5 is (√2 / 2)^2. 
-  
+  const cutoff = 0.5 * Math.pow(ray.distance, 2); // 0.5 is (√2 / 2)^2.
+
   return (orientation2 < cutoff) ? 0 : orientation;
 }
 
@@ -67,37 +67,65 @@ function orient2dPixelLine(ray, c) {
  * Is the point c within a pixel of the ray and thereby "contained" by the ray?
  * @param {Ray} ray
  * @param {Point} c
- * @return {boolean}  True if the ray contains the point c. 
+ * @return {boolean}  True if the ray contains the point c.
  */
 export function pixelLineContainsPoint(ray, c) {
   if(equivalentPixel(ray.A, c) ||
      equivalentPixel(ray.B, c)) return true;
-     
-  if(orient2dPixelLine(ray, c) !== 0) return false;   
+
+  if(orient2dPixelLine(ray, c) !== 0) return false;
 
   // test if point is between the endpoints, given we already established collinearity
   const dot = function(r1, r2) {
      return r1.dx * r2.dx + r1.dy * r2.dy;
   }
-  
+
   const AC = new Ray(ray.A, c);
   const k_ab = dot(ray, ray);
   const k_ac = dot(ray, AC);
-  
-  // if k_ac === 0, point p coincides with A (handled by prior check)    
+
+  // if k_ac === 0, point p coincides with A (handled by prior check)
   // if k_ac === k_ab, point p coincides with B (handled by prior check)
   // k_ac is between 0 and k_ab, point is on the segment
-  return k_ac >= 0 && k_ac <= k_ab;   
+  return k_ac >= 0 && k_ac <= k_ab;
 }
 
 /**
  * Compare function to sort point by x, then y coordinates
  * @param {Point} a
  * @param {Point} b
- * @return {-1|0|1} 
+ * @return {-1|0|1}
  */
-export function compareXY(a, b) { 
-  return ( a.x - b.x ) || (a.y - b.y );
+export function compareXYInt(a, b) {
+  return (a.x - b.x) || (a.y - b.y);
+}
+
+export function compareXY(a, b) {
+  const diff_x = a.x - b.x;
+  if(diff_x.almostEqual(0)) {
+    const diff_y = a.y - b.y;
+    return diff_y.almostEqual(0) ? 0 : diff_y;
+  }
+  return diff_x;
+}
+
+/**
+ * Compare function to sort point by y, then x coordinates
+ * @param {Point} a
+ * @param {Point} b
+ * @return {-1|0|1}
+ */
+export function compareYXInt(a, b) {
+  return (a.y - b.y) || (a.x - b.x);
+}
+
+export function compareYX(a, b) {
+  const diff_y = a.y - b.y;
+  if(diff_y.almostEqual(0)) {
+    const diff_x = a.x - b.x;
+    return diff_x.almostEqual(0) ? 0 : diff_x;
+  }
+  return diff_y;
 }
 
 /**
@@ -116,12 +144,16 @@ export function randomPositiveZeroInteger(max) {
 export function keyForPoint(p) {
   return (Math.round(p.x) << 16) ^ Math.round(p.y);
  }
- 
+
+function keyForSegment(s) {
+  return (BigInt(s.nw.key) << 32n) ^ BigInt(s.se.key);
+}
+
 /**
  * Test if a line (not a segment) blocks a point relative to an origin
- * Used in ClockwiseSweep to test if an active edge blocks. In that case, 
+ * Used in ClockwiseSweep to test if an active edge blocks. In that case,
  * we already know the origin and point are in line with the active edge.
- * (Otherwise, it would not be an active edge.) Slightly faster than 
+ * (Otherwise, it would not be an active edge.) Slightly faster than
  * testing a segment, and about 20–25% faster than testing for intersections.
  * Another reasonably fast choice (~ 6% slower) would be to use
  * foundry.utils.lineSegmentIntersects.
@@ -135,5 +167,5 @@ export function keyForPoint(p) {
 export function lineBlocksPoint(a, b, p, o) {
    return (foundry.utils.orient2dFast(a, b, p) * foundry.utils.orient2dFast(a, b, o)) < 0;
 }
- 
-  
+
+
