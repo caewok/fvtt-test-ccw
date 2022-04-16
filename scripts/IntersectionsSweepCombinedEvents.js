81eb7401a97c63dfa5fd477c7f4293a1780bf4e6
Michael Enion <caewok_2000@yahoo.com>
2022-04-16 13:25:02 -0700

    Store indices in segments and modify when insert/swap/delete
    At insert, store the segment index.
    For binary search, O(log(n)) to find the segment location. Worst case, O(n) to update existing indices. 
    
    At swap/reverse, move the segment index.
    Only need to update those segments that are swapped. O(~1)
    
    At delete, remove the segment index. Worst case, O(n) to update existing indices. 
    
    For updating, average will be much less than O(n). Maybe O(n / 2).

--- scripts/IntersectionsSweepCombinedEvents.js
+++ scripts/IntersectionsSweepCombinedEvents.js
@@ -12,7 +12,7 @@ import { MODULE_ID, UseBinary } from "./module.js";
 import { OrderedArray } from "./OrderedArray.js";
 import { binaryFindIndex, binaryIndexOf } from "./BinarySearch.js";
 import { drawVertex, drawEdge, COLORS, clearLabels, labelVertex } from "./Drawing.js";
-import { EventType, hashSegments, pointForSegmentGivenX, SegmentArray } from "./IntersectionsSweep.js";
+import { EventType, hashSegments } from "./IntersectionsSweep.js";
 
 
 export function findIntersectionsSweepCombinedSingle(segments, reportFn = (e1, e2, ix) => {}) {
@@ -57,7 +57,7 @@ export function findIntersectionsSweepCombinedSingle(segments, reportFn = (e1, e
         break;
     }
 
-    if(debug) { console.table(tree.data, ["_id"]); }
+    if(debug) { console.table(tree.data, ["_id", "_idx"]); }
 
   }
 
@@ -67,31 +67,29 @@ export function findIntersectionsSweepCombinedSingle(segments, reportFn = (e1, e
 function handleLeftEvent(curr, e, tree, tracker) {
   const debug = game.modules.get(MODULE_ID).api.debug;
   let num_ixs = 0;
+  const sweep_x = curr.point.x;
 
-  // insert each left segment and get their indices
-  // then get above/below the min/max indices
-  let max_idx = Number.NEGATIVE_INFINITY;
-  let min_idx = Number.POSITIVE_INFINITY;
-  for(const s of curr.segments) {
+  // insert each left segment
+  const segmentSet = curr.segments;
+  segmentSet.forEach(s => {
+    tree.insert(s, sweep_x);
     if(debug) {
-      console.log(`\tLeft endpoint event for ${s.nw.x},${s.nw.y}|${s.se.x},${s.se.y}`);
+      console.log(`\tAdding ${s.nw.x},${s.nw.y}|${s.se.x},${s.se.y}`);
       drawEdge(s);
     }
+  });
 
-    const idx = tree.insert(s, curr.point.x);
-    max_idx = Math.max(max_idx, idx);
-    min_idx = Math.min(min_idx, idx);
+  // and find the above/below values
+  let { above, below, min_idx, max_idx } = segmentIndexSpread(segmentSet, tree);
+  if(below) {
+    let bottom_segment = tree.atIndex(max_idx);
+    num_ixs += checkForIntersection(below, bottom_segment, e, tracker);
   }
 
-  // check if curr intersects with its predecessor and successor
-  // if we already checked this pair, we can skip
-  let below = tree.successor(max_idx); // below means higher index, so successor
-  let bottom_segment = tree.atIndex(max_idx);
-  if(below) { num_ixs += checkForIntersection(below, bottom_segment, e, tracker); }
-
-  let above = tree.predecessor(min_idx);
-  let top_segment = tree.atIndex(max_idx);
-  if(above) { num_ixs += checkForIntersection(above, top_segment, e, tracker); }
+  if(above) {
+    let top_segment = tree.atIndex(min_idx);
+    num_ixs += checkForIntersection(above, top_segment, e, tracker);
+  }
 
   return num_ixs;
 }
@@ -99,41 +97,48 @@ function handleLeftEvent(curr, e, tree, tracker) {
 function handleIntersectionEvent(curr, e, tree, tracker, reportFn) {
   const debug = game.modules.get(MODULE_ID).api.debug;
   let num_ixs = 0;
+  let segmentSet = curr.segments;
+
+  if(debug) {
+    console.log(`\tIntersection event ${curr.point.x},${curr.point.y}`);
+    drawVertex(curr.point);
+    console.log(`\tSwapping ${segmentSet.size} segments.`);
+  }
 
   // report intersection
   // for now, if multiple segments, report every combination
-  let segmentSet = curr.segments;
+
   let segmentArr = [...segmentSet];
   let ln = segmentArr.length;
   for(let i = 0; i < ln; i += 1) {
     const si = segmentArr[i];
     for(let j = (i + 1); j < ln; j += 1) {
       const sj = segmentArr[j];
+      if(debug) { console.log(`\tReporting ${si.nw.x},${si.nw.y}|${si.se.x},${si.se.y} x ${sj.nw.x},${sj.nw.y}|${sj.se.x},${sj.se.y}`); }
       reportFn(si, sj, curr.point);
     }
   }
 
+
+
+
   // find all intersecting indices and the bracketing segments
-  let [s0] = segmentSet;
-  let idx = sweepIndex(s0, curr.point.x, tree);
-  let { above, below, min_idx, max_idx } = segmentIndexSpread(segmentSet, idx, tree);
-
-
-  if(debug) {
-    console.log(`\tIntersection event ${curr.point.x},${curr.point.y}`);
-    drawVertex(curr.point);
-    console.log(`\tSwapping ${segmentSet.size} segments. First is ${s0.nw.x},${s0.nw.y}|${s0.se.x},${s0.se.y}`);
-  }
+  let { above, below, min_idx, max_idx } = segmentIndexSpread(segmentSet, tree);
 
   // reverse (swap) indices
   tree.reverseIndices(min_idx, max_idx);
 
+
   // check for intersection between the upper segment and above
   // and between lower segment and below (after the swap/reversal)
-  const bottom_segment = tree.atIndex(max_idx);
-  const top_segment = tree.atIndex(min_idx);
-  if(below) { num_ixs += checkForIntersection(below, bottom_segment, e, tracker); }
-  if(above) { num_ixs += checkForIntersection(above, top_segment, e, tracker); }
+  if(below) {
+    const bottom_segment = tree.atIndex(max_idx);
+    num_ixs += checkForIntersection(below, bottom_segment, e, tracker);
+  }
+  if(above) {
+    const top_segment = tree.atIndex(min_idx);
+    num_ixs += checkForIntersection(above, top_segment, e, tracker);
+  }
 
   return num_ixs;
 }
@@ -154,21 +159,18 @@ function handleRightEvent(curr, e, tree, tracker) {
   // 5. delete all ending segments
 
   let segmentSet = curr.segments;
-  let [s0] = segmentSet;
-  let idx = sweepDeletionIndex(s0, curr.point.x, tree);
-  let { indices, above, below } = segmentIndexSpread(segmentSet, idx, tree);
+  if(debug) { console.log(`\tRight endpoint event for ${segmentSet.size} segments.`); }
+  let { above, below } = segmentIndexSpread(segmentSet, tree);
 
-  if(debug) {
-    console.log(`\tRight endpoint event for ${segmentSet.size} segments. First is ${s0.nw.x},${s0.nw.y}|${s0.se.x},${s0.se.y}`);
-
-    for(const s of segmentSet) {
+  if(below && above) { num_ixs += checkForIntersection(below, above, e, tracker); }
+  for(const s of segmentSet) {
+    if(debug) {
       console.log(`\tDeleting ${s.nw.x},${s.nw.y}|${s.se.x},${s.se.y}`);
       drawEdge(s, COLORS.red);
     }
-  }
 
-  if(below && above) { num_ixs += checkForIntersection(below, above, e, tracker); }
-  indices.forEach(idx => tree.deleteAtIndex(idx));
+    tree.remove(s);
+  }
 
   // do we need to delete associated ix events? (Hopefully not; that may be hard.)
   // probably handled by the tracker eliminating ix overlap
@@ -179,77 +181,21 @@ function handleRightEvent(curr, e, tree, tracker) {
 // assuming the segmentSet contains adjacent segments in the tree,
 // find the range of indices in the set as well as the above/below segments
 // bracketing the set in the ordered array
-function segmentIndexSpread(segmentSet, startIndex, tree) {
-  let indices = [startIndex];
-  let min_idx = startIndex;
-  let max_idx = startIndex;
-  let above = tree.predecessor(startIndex);
-  let below = tree.successor(startIndex);
-  if(segmentSet.size > 1) {
-    let above_incr = 1;
-    let below_incr = 1;
-    while(segmentSet.has(above)) {
-      indices.push(startIndex - above_incr);
-      min_idx = startIndex - above_incr;
-      above_incr += 1;
-      above = tree.predecessor(startIndex - above_incr);
-    }
+function segmentIndexSpread(segmentSet, tree) {
+  let min_idx = Number.POSITIVE_INFINITY;
+  let max_idx = Number.NEGATIVE_INFINITY;
+  let indices = segmentSet.forEach(s => {
+    const idx = s._idx;
+    min_idx = Math.min(idx, min_idx);
+    max_idx = Math.max(idx, max_idx);
+    return idx;
+  });
 
-    while(segmentSet.has(below)) {
-      indices.push(startIndex + below_incr);
-      max_idx = startIndex + below_incr;
-      below_incr += 1;
-      below = tree.successor(startIndex + below_incr);
-    }
-  }
+  const above = tree.predecessor(min_idx);
+  const below = tree.successor(max_idx);
 
   return { indices, above, below, min_idx, max_idx };
 }
-
-function sweepIndex(s, sweep_x, tree) {
-  const debug_binary = game.modules.get(MODULE_ID).api.debug_binary;
-  let idx, idx_bin;
-  switch(debug_binary) {
-    case UseBinary.Yes:
-      idx = tree.binaryIndexOf(s, sweep_x);
-      break;
-
-    case UseBinary.Test:
-      idx = tree.indexOf(s);
-      idx_bin = tree.binaryIndexOf(s, sweep_x);
-      if(idx !== idx_bin) { console.warn(`index segment: idx bin ${idx_bin} ≠ ${idx} at sweep ${sweep_x}`); }
-      break;
-
-    case UseBinary.No:
-      idx = tree.indexOf(s);
-      break;
-  }
-  if(!~idx) console.error("Segment not found", s);
-  return idx;
-}
-
-function sweepDeletionIndex(s, sweep_x, tree) {
-  const debug_binary = game.modules.get(MODULE_ID).api.debug_binary;
-  let idx, idx_bin;
-  switch(debug_binary) {
-    case UseBinary.Yes:
-      idx = tree.deletionBinaryIndexOf(s, sweep_x);
-      break;
-
-    case UseBinary.Test:
-      idx = tree.indexOf(s);
-      idx_bin = tree.deletionBinaryIndexOf(s, sweep_x);
-      if(idx !== idx_bin) { console.warn(`delete segment: idx bin ${idx_bin} ≠ ${idx} at sweep ${sweep_x}`); }
-      break;
-
-    case UseBinary.No:
-      idx = tree.indexOf(s);
-      break;
-  }
-  if(!~idx) console.error("Segment not found", s);
-  return idx;
-}
-
 
 
 
@@ -286,6 +232,202 @@ function checkForIntersection(s1, s2, e, tracker) {
   return num_ixs;
 }
 
+ class SegmentArray extends OrderedArray {
+
+
+ /**
+  * For a given x position, return the y position along the segment or undefined if
+  * the segment is vertical. Does not check fo whether the point is on the segment,
+  * versus beyond the segment.
+  * @param {Segment}  s   Object with A.x, A.y, B.x, and B.y coordinates.
+  * @param {Number}   x   X-coordinate from which to generate a y-coordinate.
+  * @return {Point|Undefined}
+  */
+  static pointForSegmentGivenX(s, x) {
+    const denom = s.B.x - s.A.x;
+    if(!denom) return undefined;
+    return { x: x, y: ((s.B.y - s.A.y) / denom * (x - s.A.x)) + s.A.y };
+  }
+
+ /**
+  * Take in a sweep_x to adjust the segment comparison as the sweep moves along the x axis.
+  * Otherwise like the OrderedArray version.
+  * @param {Segment}  segment   Object with A.x, A.y, B.x, B.y coordinates, plus
+  *                             _tmp_nw set when inserting in this class's array.
+  * @param {Number}   sweep_x   X-coordinate used in the comparison.
+  * @return {Number}  Index of the segment in the array.
+  */
+  binaryIndexOf(segment, sweep_x) {
+    // if segment._tmp_nw is undefined, it either has not been added or
+    // already removed.
+    if(!segment._tmp_nw) { return -1; }
+
+    // if the segment is vertical, there is a good chance we will not know its location.
+    // A vertical line may have multiple intersections at the same sweep_x.
+    // This will proceed:
+    // - identify intersection
+    // - swap intersection
+    // - identify second intersection
+    // <-- here, the segment has already been swapped but we are still at the same sweep_x,
+    //     so we would need to somehow know that to know where it is in the data array.
+    // - swap second intersection ...
+    // Instead, bail out and return a non-binary search instead
+    if(segment.A.x === segment.B.x) { return this.data.indexOf(segment); }
+
+    segment._tmp_nw = SegmentArray.pointForSegmentGivenX(segment, sweep_x) || segment._tmp_nw; // if vertical, use existing
+    return binaryIndexOf(this.data, segment, (a, b) => this._segmentCompare(a, b, sweep_x));
+  }
+
+ /**
+  * Insert an object in the array.
+  * Takes an x coordinate to insert at the correct position given the sweep location.
+  * @param {Object}   obj      Object with A.x, A.y, B.x, B.y coordinates, plus a nw
+  *                            property indicating whether A or B are more nw.
+  * @param {Number}   sweep_x Sweep location
+  * @return {number}  Index where the object was inserted.
+  */
+  insert(segment, sweep_x) {
+    const debug_binary = game.modules.get(MODULE_ID).api.debug_binary;
+    if(typeof sweep_x === "undefined") { console.warn("insert requires sweep_x"); }
+
+    // add a temporary index flag, because during the sweep segments are swapped at
+    // intersections, thus switching their "y" values accordingly.
+    segment._tmp_nw = segment.nw;
+
+    // for debugging
+    let idx, idx_bin;
+    switch(debug_binary) {
+      case UseBinary.Yes:
+        idx = binaryFindIndex(this.data, elem => this._elemIsAfter(segment, elem, sweep_x));
+        break;
+
+      case UseBinary.Test:
+        idx = this.data.findIndex(elem => this._elemIsAfter(segment, elem, sweep_x));
+        idx_bin = binaryFindIndex(this.data, elem => this._elemIsAfter(segment, elem, sweep_x));
+        if(idx !== idx_bin) { console.warn(`insert segment: idx bin ${idx_bin} ≠ ${idx} at sweep ${sweep_x}`); }
+        break;
+
+      case UseBinary.No:
+        idx = this.data.findIndex(elem => this._elemIsAfter(segment, elem, sweep_x));
+        break;
+    }
+
+    segment._idx = this._insertAt(segment, idx);
+    // only way to fix the indices of the remainder appears to be to
+    // update them sequentially
+    const ln = this.data.length;
+    for(let i = segment._idx + 1; i < ln; i += 1) {
+      this.data[i]._idx += 1;
+    }
+
+    return segment._idx;
+
+  }
+
+ /**
+  * Swap two segments in the array.
+  */
+  swap(segment1, segment2) {
+    if(!segment1._tmp_nw || !segment2._tmp_nw) { return -1; }
+
+    const idx1 = segment1._idx;
+    const idx2 = segment2._idx;
+
+    if(!~idx1 || !~idx2) {
+//       console.warn("swap segments not found.");
+      return [undefined, undefined];
+    }
+
+    // change their temporary values (only *after* finding their current index)
+
+    [ segment2._tmp_nw, segment1._tmp_nw ] = [ segment1._tmp_nw, segment2._tmp_nw ];
+
+    // change their position
+    //this.swapIndices(idx1, idx2);
+    this.data[idx1] = segment2;
+    this.data[idx2] = segment1;
+
+    segment1._idx = idx2;
+    segment2._idx = idx1;
+
+    return [idx2, idx1];
+  }
+
+ /**
+  * Reverse all objects from idx1 to idx2.
+  * Dangerous! If comparator does not accommodate the swap, binary searching may
+  * return unpredictable results and insertion/deletion (binary or non-binary) may also
+  * fail.
+  * @param {number} idx1    Index of first object to reverse in array
+  * @param {number} idx2    Index of last object to reverse in array
+  */
+  reverseIndices(start_idx, end_idx) {
+    const arr_to_reverse = this.data.slice(start_idx, end_idx + 1);
+    arr_to_reverse.reverse();
+    this.data = this.data.slice(0, start_idx).concat(arr_to_reverse, this.data.slice(end_idx + 1));
+
+    for(let i = start_idx; i <= end_idx; i += 1) {
+      this.data[i]._idx = i;
+    }
+  }
+
+ /**
+  * Delete a segment
+  */
+  remove(segment) {
+    const idx = segment._idx;
+
+    if(typeof idx === "undefined") {
+      console.log(`Attempted removal of segment that has no index`, segment);
+      return;
+    }
+    this.data.splice(idx, 1);
+
+    // only way to fix the indices of the remainder appears to be to
+    // update them sequentially
+    const ln = this.data.length;
+    for(let i = idx; i < ln; i += 1) {
+      this.data[i]._idx -= 1;
+    }
+
+    segment._tmp_nw = undefined;
+    segment._idx = undefined;
+  }
+
+ /**
+  * Comparison function that uses a changeable sweep_x value
+  * to adjust the comparison as the sweep moves across the x axis.
+  */
+  _segmentCompare(segment, elem, sweep_x) {
+    // use the current sweep location to set nw for each existing element
+    elem._tmp_nw = SegmentArray.pointForSegmentGivenX(elem, sweep_x) || elem._tmp_nw; // if vertical keep existing
+
+    // if compareXY is not 0, use that comparison result.
+    // otherwise, segment and elem share nw endpoint or se endpoint equals _tmp_nw at sweep
+    // sort by extending the lines to the nw: whichever is higher in the nw
+    // direction is first in the sort.
+    // can determine this directly by extending lines from the se, or indirectly by
+    // determining their orientation relative to one another
+
+    // if they share the nw endpoint, the first orientation will return 0; test again
+    // in opposite direction. So first orientation tests > shape; second tests < shape
+    // of the X.
+
+    return compareYX(segment._tmp_nw, elem._tmp_nw) ||
+           foundry.utils.orient2dFast(elem.se, elem.nw, segment.nw) ||
+           foundry.utils.orient2dFast(elem.nw, elem.se, segment.se);
+  }
+
+ /**
+  * Helper function transforming the comparator output to true/false; used by insert.
+  * @param {Object}   obj     Object to search for
+  * @param {Object}   elem    Element of the array
+  * @param {number}   sweep_x Position of the sweep
+  * @return {boolean} True if the element is after the segment in the ordered array.
+  */
+  _elemIsAfter(segment, elem, sweep_x) { return this._segmentCompare(segment, elem, sweep_x) < 0; }
+
+}
 
 
 class EventTypeClass {
