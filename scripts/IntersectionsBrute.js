3c9e0946666cbf525aea65823257b8e7fcdf0237
Michael Enion <caewok_2000@yahoo.com>
2022-04-26 15:49:40 -0700

    Move identifyIntersection helper functions to utilities

--- scripts/IntersectionsBrute.js
+++ scripts/IntersectionsBrute.js
@@ -1,7 +1,5 @@
 /* globals
-
 foundry
-
 */
 
 'use strict';
@@ -9,111 +7,10 @@ foundry
 import { compareXY } from "./utilities.js";
 
 
-/*
-Testing
-
-// Generate a set of random segments; compare intersection reporting between versions
-// use a custom reporting function to capture the ix
-
-reporting_arr_brute = []
-reporting_arr_sort = []
-reporting_arr_sweep = []
-
-function randomPoint(max_coord) {
-  return { x: Math.floor(Math.random() * max_coord),
-           y: Math.floor(Math.random() * max_coord) };
-}
-function randomSegment(max_coord = 5000) {
-    return new SimplePolygonEdge(randomPoint(max_coord), randomPoint(max_coord));
-}
-function pointsEqual(p1, p2) { return (p1.x.almostEqual(p2.x) && p1.y.almostEqual(p2.y)) }
-
-
-reportFnBrute = (s1, s2) => {
-  const x = foundry.utils.lineLineIntersection(s1.A, s1.B, s2.A, s2.B);
-  reporting_arr_brute.push(x);
-}
-
-reportFnSort = (s1, s2) => {
-  const x = foundry.utils.lineLineIntersection(s1.A, s1.B, s2.A, s2.B);
-  reporting_arr_sort.push(x);
-}
-
-reportFnSweep = (s1, s2, ix) => {
-  reporting_arr_sweep.push(ix);
-}
-
-reporting_arr_brute = []
-reporting_arr_sort = []
-reporting_arr_sweep = []
-
-segments = Array.fromRange(10).map(i => randomSegment(5000))
-canvas.controls.debug.clear()
-segments.forEach(s => drawEdge(s, COLORS.black))
-
-findIntersectionsBruteSingle(segments, reportFnBrute)
-findIntersectionsSortSingle(segments, reportFnSort)
-processIntersections(segments, reportFnSweep)
-
-reporting_arr_brute.sort(compareXY)
-reporting_arr_sort.sort(compareXY)
-reporting_arr_sweep.sort(compareXY)
-
-reporting_arr_brute.every((pt, idx) => pointsEqual(pt, reporting_arr_sort[idx]))
-reporting_arr_brute.every((pt, idx) => pointsEqual(pt, reporting_arr_sweep[idx]))
-
-segments = [];
-for(let i = 0; i < 1000; i += 1) {
-  reporting_arr_brute = []
-  reporting_arr_sort = []
-  reporting_arr_sweep = []
-
-  segments = Array.fromRange(100).map(i => randomSegment(5000))
-
-  findIntersectionsBruteSingle(segments, reportFnBrute)
-  findIntersectionsSortSingle(segments, reportFnSort)
-  processIntersections(segments, reportFnSweep)
-
-  reporting_arr_brute.sort(compareXY)
-  reporting_arr_sort.sort(compareXY)
-  reporting_arr_sweep.sort(compareXY)
-
-  const brute_sort_test = reporting_arr_brute.length === reporting_arr_sort.length &&
-    reporting_arr_brute.every((pt, idx) => pointsEqual(pt, reporting_arr_sort[idx]))
-  const brute_sweep_test = reporting_arr_brute.length === reporting_arr_sweep.length &&
-    reporting_arr_brute.every((pt, idx) => pointsEqual(pt, reporting_arr_sweep[idx]))
-
-  if(!brute_sort_test) {
-    console.error(`Failed brute sort`)
-  } else if (!brute_sweep_test) {
-    console.error(`Failed brute sweep`)
-  }
-
-}
-
-
-*/
 
 
 
 
-export function identifyIntersectionsWith(s1, s2) {
-  if(s1 === s2) return; // probably unnecessary
-
-  const {a: a1, b: b1} = s1.vertices;
-  const {a: a2, b: b2} = s2.vertices;
-
-  const x = foundry.utils.lineLineIntersection(s1.A, s1.B, s2.A, s2.B);
-  if(!x) return; // may not be necessary but may eliminate colinear lines
-  s1.intersectsWith.set(s2, x);
-  s2.intersectsWith.set(s1, x);
-}
-
-export function identifyIntersectionsWithNoEndpoint(s1, s2) {
-  if(s1.wallKeys.intersects(s2.wallKeys)) return;
-
-  return identifyIntersectionsWith(s1, s2);
-}
 
 
 /**
@@ -148,7 +45,7 @@ export function findIntersectionsBruteSingle(edges, reportFn = (e1, e2) => {}) {
  * Intersections marked in the set
  * Comparable to identifyWallIntersections method from WallsLayer Class
  */
-export function findIntersectionsBruteRedBlack(edges1, edges2k, reportFn = (e1, e2) => {}) {
+export function findIntersectionsBruteRedBlack(edges1, edges2, reportFn = (e1, e2) => {}) {
   const ln1 = edges1.length;
   const ln2 = edges2.length;
   if(!ln1 || !ln2) return;
