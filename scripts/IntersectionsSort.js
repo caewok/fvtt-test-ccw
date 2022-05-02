563311c9d6959925a74c4234822523799f614674
Michael Enion <caewok_2000@yahoo.com>
2022-05-02 07:22:37 -0700

    Foundry eslint intersectionssort.js

--- scripts/IntersectionsSort.js
+++ scripts/IntersectionsSort.js
@@ -2,9 +2,9 @@
 foundry
 */
 
-/*eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }]*/
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 
-'use strict';
+"use strict";
 
 /*
 Report intersections between segments using a near-brute force algorithm that
@@ -38,7 +38,7 @@ export function findIntersectionsSortSingle(segments, reportFn = (_s1, _s2) => {
 
   // In a single pass through the array, build an array of endpoint objects.
   // Each object contains an endpoint, a link to the underlying segment, and a boolean
-  // indicator for whether it is the nw or se endpoint.
+  // Indicator for whether it is the nw or se endpoint.
   // Sort the new array by the x values, breaking ties by sorting the se point first.
   // (it is fine if two segments are otherwise equivalent in the sort)
 
@@ -46,27 +46,27 @@ export function findIntersectionsSortSingle(segments, reportFn = (_s1, _s2) => {
   for (let i = 0; i < ln; i += 1) {
     const s = segments[i];
     endpoints.push({e: s.nw, s, se: -1},
-                   {e: s.se, s, se: 1});
+                   {e: s.se, s, se: 1}); // eslint-disable-line indent
   }
   endpoints.sort(sortEndpoints);
 
   const ln2 = endpoints.length;
   for (let i = 0; i < ln2; i += 1) {
     const endpoint1 = endpoints[i];
-    if (~endpoint1.se) continue; // avoid duplicating the check
+    if (~endpoint1.se) continue; // Avoid duplicating the check
 
-    // starting j is always i + 1 b/c any segment with an se endpoint after si
+    // Starting j is always i + 1 b/c any segment with an se endpoint after si
     // would be after si or already processed b/c its ne endpoint was before.
     const start_j = i + 1;
     const si = endpoint1.s;
     for (let j = start_j; j < ln2; j += 1) {
       const endpoint2 = endpoints[j];
 
-      if (endpoint2.e.x > si.se.x) break; // segments past here are entirely right of si
+      if (endpoint2.e.x > si.se.x) break; // Segments past here are entirely right of si
       if (~endpoint2.se) continue;
 
       const sj = endpoint2.s;
-      foundry.utils.lineSegmentIntersects(si.A, si.B, sj.A, sj.B) && reportFn(si, sj);
+      foundry.utils.lineSegmentIntersects(si.A, si.B, sj.A, sj.B) && reportFn(si, sj); // eslint-disable-line no-unused-expressions
     }
   }
 }
@@ -83,11 +83,11 @@ export function findIntersectionsSortSingle(segments, reportFn = (_s1, _s2) => {
  *                  < 0: sort e1 before e2
  */
 function sortEndpoints(e1, e2) {
-  return e1.e.x - e2.e.x ||
-         e2.se - e1.se;
+  return e1.e.x - e2.e.x
+         || e2.se - e1.se;
 
-  // if e1.se then we want e1 first or they are equal. So return -
-  // if e2.se then we want e2 first or they are equal. So return +
+  // If e1.se then we want e1 first or they are equal. So return -
+  // If e2.se then we want e2 first or they are equal. So return +
   // e2.se - e1.se
   // e1.se: -1, e2.se: 1. 1 - - 1 = 2; e2 first
   // e1.se: 1, e2.se: -1. -1 - 1 = -2:; e1 first
@@ -124,37 +124,37 @@ export function findIntersectionsSortRedBlack(red, black, reportFn = (_s1, _s2) 
   for (let i = 0; i < ln_red; i += 1) {
     const s = red[i];
     endpoints.push({e: s.nw, s, se: -1, red: true},
-                   {e: s.se, s, se: 1,  red: true});
+                   {e: s.se, s, se: 1,  red: true}); // eslint-disable-line indent, no-multi-spaces
   }
   for (let i = 0; i < ln_black; i += 1) {
     const s = black[i];
     endpoints.push({e: s.nw, s, se: -1, red: false},
-                   {e: s.se, s, se: 1,  red: false});
+                   {e: s.se, s, se: 1,  red: false}); // eslint-disable-line indent, no-multi-spaces
   }
 
   endpoints.sort(sortEndpoints);
   const ln_endpoints = endpoints.length;
   for (let i = 0; i < ln_endpoints; i += 1) {
     const endpoint1 = endpoints[i];
-    if (~endpoint1.se) continue; // avoid duplicating the check
+    if (~endpoint1.se) continue; // Avoid duplicating the check
 
-    // starting j is always i + 1 b/c any segment with an se endpoint after si
-    // would be after si or already processed b/c its ne endpoint was before.
+    // Starting j is always i + 1 b/c any segment with an se endpoint after si
+    // Would be after si or already processed b/c its ne endpoint was before.
     const start_j = i + 1;
     const si = endpoint1.s;
     for (let j = start_j; j < ln_endpoints; j += 1) {
       const endpoint2 = endpoints[j];
 
-      if (endpoint2.e.x > si.se.x) break; // segments past here are entirely right of si
-      if (!(endpoint1.red ^ endpoint2.red)) continue; // only want segments of different color
+      if (endpoint2.e.x > si.se.x) break; // Segments past here are entirely right of si
+      if (!(endpoint1.red ^ endpoint2.red)) continue; // Only want segments of different color
       if (~endpoint2.se) continue;
 
       const sj = endpoint2.s;
-      foundry.utils.lineSegmentIntersects(si.A, si.B, sj.A, sj.B) && reportFn(si, sj);
+      foundry.utils.lineSegmentIntersects(si.A, si.B, sj.A, sj.B) && reportFn(si, sj); // eslint-disable-line no-unused-expressions
     }
   }
 }
-// testing:
+// Testing:
 // reportFn = (_s1, _s2) => { console.log(`${_s1.id} x ${_s2.id}`) }
 
 // TO-DO: Version of RedBlack that uses an existing sorted endpoint list,
