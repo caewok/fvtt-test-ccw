d6bd55d802371868beb1263a9be9f12e126c27bc
Michael Enion <caewok_2000@yahoo.com>
2022-05-01 18:13:48 -0700

    Foundry eslint for binarysearch.js

--- scripts/BinarySearch.js
+++ scripts/BinarySearch.js
@@ -1,5 +1,8 @@
 /* Functions to binary search a sorted array.
-Binary, Interpolate, and InterpolateBinary options
+Binary, Interpolate, and InterpolateBinary options.
+For each, there are versions to work with an array of numbers and an array of objects
+that can be scored.
+
 • Binary halves the search each time from the midpoint. O(log(n))
 • Interpolate halves the search from a point based on
   where the target value is likely to be given min and max of the array
@@ -26,8 +29,6 @@ Type of searching:
              element in the sorted array is true.
 
 */
-// - indexOf: Operates like
-
 
 /**
  * Find the index of a value in a sorted array of numbers.
@@ -47,12 +48,12 @@ export function binaryIndexOf(arr, x) {
   let start = 0;
   let end = arr.length - 1;
 
-  // iterate, halving the search each time
+  // Iterate, halving the search each time.
   while (start <= end) {
     const mid = Math.floor((start + end) / 2);
-    if(arr[mid] === x) return mid;
+    if (arr[mid] === x) return mid;
 
-    if(arr[mid] < x) {
+    if (arr[mid] < x) {
       start = mid + 1;
     } else {
       end = mid - 1;
@@ -81,13 +82,13 @@ export function binaryIndexOfObject(arr, obj, cmpFn = (a, b) => a - b) {
   let start = 0;
   let end = arr.length - 1;
 
-  // iterate, halving the search each time
+  // Iterate, halving the search each time.
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
     let res = cmpFn(obj, arr[mid], mid);
-    if(!res) { return mid; }
+    if (!res) { return mid; }
 
-    if(res > 0) {
+    if (res > 0) {
       start = mid + 1;
     } else {
       end = mid - 1;
@@ -129,62 +130,38 @@ export function binaryFindIndex(arr, comparator) {
   let end = ln - 1;
   let mid = -1;
 
-  // need first index for which callbackFn returns true
+  // Need first index for which callbackFn returns true
   // b/c the array is sorted, once the callbackFn is true for an index,
-  // it is assumed true for the rest
-  // so, e.g, [F,F,F, T, T, T, T]
-  // progressively check until we have no items left.
+  // it is assumed true for the rest.
+  // So, e.g, [F,F,F, T, T, T, T]
+  // Progressively check until we have no items left.
 
-  // Iterate, halving the search each time we find a true value
+  // Iterate, halving the search each time we find a true value.
   let last_true_index = -1;
-  while (start <= end){
-    // find the mid index
+  while (start <= end) {
+    // Find the mid index.
     mid = Math.floor((start + end) / 2);
 
-    // determine if this index returns true
+    // Determine if this index returns true.
     const res = comparator(arr[mid], mid);
 
-    if(res) {
-      // if the previous is false, we are done
-      if((mid - 1) >= 0 && !comparator(arr[mid - 1], mid - 1)) { return mid; }
-      // if we found a true value, we can ignore everything after mid
+    if (res) {
+      // If the previous is false, we are done.
+      if ((mid - 1) >= 0 && !comparator(arr[mid - 1], mid - 1)) { return mid; }
+      // If we found a true value, we can ignore everything after mid
       last_true_index = mid;
       end = mid - 1;
     } else {
-      // if the next value is true, we are done
-      if((mid + 1) < ln && comparator(arr[mid + 1], mid + 1)) { return mid + 1; }
-      // otherwise, the first true value might be after mid
-      // (b/c it is sorted, it cannot be before)
+      // If the next value is true, we are done.
+      if ((mid + 1) < ln && comparator(arr[mid + 1], mid + 1)) { return mid + 1; }
+      // Otherwise, the first true value might be after mid.
+      // (b/c it is sorted, it cannot be before.)
       start = mid + 1;
     }
   }
 
   return last_true_index;
 }
-
-// let arr = Array.fromRange(1000).map(elem => Math.random())
-// arr.sort((a, b) => a - b)
-// for(let i = 0; i < 1000; i += 1) {
-//   let n = Math.random();
-//   let i0 = arr.findIndex(elem => elem > n) - 1;
-//   let i1 = binaryFindIndex(arr, elem => elem > n) - 1
-//   let i2 = interpolationFindIndexBeforeScalar(arr, n);
-//   let i3 = interpolateBinaryFindIndexBeforeScalar(arr, n);
-//   if(i0 !== i1) {
-//     console.log(`binaryFindIndex Fail at ${n}`)
-//     break;
-//   }
-//   if(i0 !== i2) {
-//     // may be different at the very end of the array: elem > n not same as elem < n && arr[idx + 1] > n
-//     console.log(`interpolateFindIndex Fail at ${n}`)
-//     break;
-//   }
-//   if(i0 !== i3) {
-//     console.log(`interpolateBinaryFindIndex Fail at ${n}`)
-//     break;
-//   }
-// }
-
 
 /**
  * Find the index of an object in a sorted array of numbers
@@ -206,20 +183,20 @@ export function interpolationIndexOf(arr, x) {
   let end = arr.length - 1;
   let position = -1;
   let delta = -1;
-  while(start <= end) {
+  while (start <= end) {
     const v_start = arr[start];
-    const v_end   = arr[end];
-    if(x < v_start || x > v_end) { break; }
+    const v_end = arr[end];
+    if (x < v_start || x > v_end) { break; }
 
     delta = (x - v_start) / (v_end - v_start);
     position = start + Math.floor((end - start) * delta);
     const v_position = arr[position];
 
-    if(v_position === x) {
+    if (v_position === x) {
       return position;
     }
 
-    if(v_position < x) {
+    if (v_position < x) {
       start = position + 1;
     } else {
       end = position - 1;
@@ -247,26 +224,24 @@ export function interpolationIndexOf(arr, x) {
  * interpolationIndexOf(arr, 2, cmpNum)
  * arr.indexOf(2)
  */
-export function interpolationIndexOfObject(arr, obj, valuationFn = (a) => a) {
+export function interpolationIndexOfObject(arr, obj, valuationFn = a => a) {
   let start = 0;
   let end = arr.length - 1;
   let position = -1;
   let delta = -1;
   let target = valuationFn(obj);
-  while(start <= end) {
+  while (start <= end) {
     const v_start = valuationFn(arr[start]);
-    const v_end   = valuationFn(arr[end]);
-    if(target < v_start || target > v_end) { break; }
+    const v_end = valuationFn(arr[end]);
+    if (target < v_start || target > v_end) { break; }
 
     delta = (target - v_start) / (v_end - v_start);
     position = start + Math.floor((end - start) * delta);
     const v_position = valuationFn(arr[position]);
 
-    if(v_position === target) {
-      return position;
-    }
+    if (v_position === target) { return position; }
 
-    if(v_position < target) {
+    if (v_position < target) {
       start = position + 1;
     } else {
       end = position - 1;
@@ -295,20 +270,20 @@ export function interpolationFindIndexBeforeScalar(arr, x) {
   let start = 0;
   let end = arr.length - 1;
 
-  if(x > arr[end]) return end;
-  if(x < arr[0]) return -1;
+  if (x > arr[end]) return end;
+  if (x < arr[0]) return -1;
 
-  while(start <= end) {
+  while (start <= end) {
     const delta = (x - arr[start]) / (arr[end] - arr[start]);
     const position = start + Math.floor((end - start) * delta);
 
-    if(arr[position] === x) return position - 1;
+    if (arr[position] === x) return position - 1;
 
-    if(arr[position] < x) {
-      if(arr[position + 1] > x) return position;
+    if (arr[position] < x) {
+      if (arr[position + 1] > x) return position;
       start = position + 1;
-    } else { // arr[position > x]
-      if(arr[position - 1] < x) return position - 1;
+    } else {
+      if (arr[position - 1] < x) return position - 1;
       end = position - 1;
     }
   }
@@ -331,73 +306,68 @@ export function interpolationFindIndexBeforeScalar(arr, x) {
  * arr.sort(cmpNum)
  * interpolationFindIndexBeforeObj(arr, 2.5)
  */
-export function interpolationFindIndexBeforeObject(arr, obj, valuationFn = (a) => a) {
+export function interpolationFindIndexBeforeObject(arr, obj, valuationFn = a => a) {
   let start = 0;
   let end = arr.length - 1;
   const x = valuationFn(obj);
 
-  if(x > valuationFn(arr[end])) return end;
-  if(x < valuationFn(arr[0])) return -1;
+  if (x > valuationFn(arr[end])) return end;
+  if (x < valuationFn(arr[0])) return -1;
 
-  while(start <= end) {
+  while (start <= end) {
     const v_start = valuationFn(arr[start]);
-    const v_end = valuationFn(arr[end])
+    const v_end = valuationFn(arr[end]);
 
     const delta = (x - v_start) / (v_end - v_start);
     const position = start + Math.floor((end - start) * delta);
     const v_position = valuationFn(arr[position]);
 
-    if(v_position === x) return position - 1;
+    if (v_position === x) return position - 1;
 
-    if(v_position < x) {
-      if(valuationFn(arr[position + 1]) > x) return position;
+    if (v_position < x) {
+      if (valuationFn(arr[position + 1]) > x) return position;
       start = position + 1;
-    } else { // arr[position > x]
-      if(valuationFn(arr[position - 1]) < x) return position - 1;
+    } else {
+      if (valuationFn(arr[position - 1]) < x) return position - 1;
       end = position - 1;
     }
   }
   return -1;
 }
 
-
-
-
-
 /**
  * Find the index of a value in a sorted array of numbers.
  * Comparable to Array.indexOf but uses interpolation and binary search together.
  * Expected time of between O(log(n)) and O(log(log(n))).
+ * Implements https://www.sciencedirect.com/science/article/pii/S221509862100046X
  * @param {Number[]}  arr   Array to search. Must be sorted low to high.
  * @param {Number}    x     Value to locate
  * @return {Number|-1} Index of the value or -1 if not found
  */
-
-// Implements https://www.sciencedirect.com/science/article/pii/S221509862100046X
 export function interpolateBinaryIndexOf(arr, x) {
   let left = 0;
   let right = arr.length - 1;
 
-  if(x > arr[right] || x < arr[0]) { return -1; }
+  if (x > arr[right] || x < arr[0]) { return -1; }
 
-  while(left < right) {
-    if(x < arr[left] || x > arr[right]) { break; }
+  while (left < right) {
+    if (x < arr[left] || x > arr[right]) { break; }
 
     const inter = left + Math.ceil((x - arr[left]) / (arr[right] - arr[left]) * (right - left));
 
-    if(x === arr[inter]) {
+    if (x === arr[inter]) {
       return inter;
-    } else if(x > arr[inter]) {
-      const mid = Math.floor((inter + right) / 2)
-      if(x <= arr[mid]) {
+    } else if (x > arr[inter]) {
+      const mid = Math.floor((inter + right) / 2);
+      if (x <= arr[mid]) {
         left = inter + 1;
         right = mid;
       } else {
         left = mid + 1;
       }
-    } else { //if(x < arr[inter]) { // if(x < arr[inter])
+    } else {
       const mid = Math.floor((inter + left) / 2);
-      if(x >= arr[mid]) {
+      if (x >= arr[mid]) {
         left = mid;
         right = inter - 1;
       } else {
@@ -406,9 +376,7 @@ export function interpolateBinaryIndexOf(arr, x) {
     }
   }
 
-  console.log(`left: ${left} right: ${right}`)
-
-  if(x === arr[left]) { return left; }
+  if (x === arr[left]) { return left; }
   return -1;
 }
 
@@ -416,37 +384,36 @@ export function interpolateBinaryIndexOf(arr, x) {
  * Find the index of a value in a sorted array of numbers.
  * Comparable to Array.indexOf but uses interpolation and binary search together.
  * Expected time of between O(log(n)) and O(log(log(n))).
+ * Implements https://www.sciencedirect.com/science/article/pii/S221509862100046X
  * @param {Number[]}  arr   Array to search. Must be sorted low to high.
  * @param {Number}    x     Value to locate
  * @return {Number|-1} Index of the value or -1 if not found
  */
-
-// Implements https://www.sciencedirect.com/science/article/pii/S221509862100046X
 export function interpolateBinaryFindIndexBeforeScalar(arr, x) {
   let left = 0;
   let right = arr.length - 1;
 
-  if(x > arr[right]) { return right; }
-  if(x < arr[0]) { return -1; }
+  if (x > arr[right]) { return right; }
+  if (x < arr[0]) { return -1; }
 
-  while(left < right) {
-    if(x < arr[left] || x > arr[right]) { break; }
+  while (left < right) {
+    if (x < arr[left] || x > arr[right]) { break; }
 
     const inter = left + Math.ceil((x - arr[left]) / (arr[right] - arr[left]) * (right - left));
 
-    if(x === arr[inter]) {
+    if (x === arr[inter]) {
       return inter - 1;
-    } else if(x > arr[inter]) {
-      const mid = Math.floor((inter + right) / 2)
-      if(x <= arr[mid]) {
+    } else if (x > arr[inter]) {
+      const mid = Math.floor((inter + right) / 2);
+      if (x <= arr[mid]) {
         left = inter + 1;
         right = mid;
       } else {
         left = mid + 1;
       }
-    } else { //if(x < arr[inter]) { // if(x < arr[inter])
+    } else {
       const mid = Math.floor((inter + left) / 2);
-      if(x >= arr[mid]) {
+      if (x >= arr[mid]) {
         left = mid;
         right = inter - 1;
       } else {
@@ -456,52 +423,52 @@ export function interpolateBinaryFindIndexBeforeScalar(arr, x) {
   }
 
 
-  if(x > arr[left - 1] && x < arr[left]) { return left - 1; }
-  if(x > arr[right] && x < arr[right + 1]) { return right; }
+  if (x > arr[left - 1] && x < arr[left]) { return left - 1; }
+  if (x > arr[right] && x < arr[right + 1]) { return right; }
 
   return -1;
 }
 
-// for(let i = 0; i < 1000; i += 1) {
-//   let n = Math.random();
-//   let i1 = binaryFindIndex(arr, elem => elem > n) - 1
-//   let i2 = interpolateBinaryFindIndexBeforeScalar(arr, n);
-//   if(i1 !== i2) {
-//     console.log(`Fail at ${n}`)
-//     break;
-//   }
-// }
-
-
-export function interpolateBinaryFindIndexBeforeObject(arr, obj, valuationFn = (a) => a) {
+/**
+ * Find the index of a value in a sorted array of objects that can be scored.
+ * Comparable to Array.indexOf but uses interpolation and binary search together.
+ * Expected time of between O(log(n)) and O(log(log(n))).
+ * Implements https://www.sciencedirect.com/science/article/pii/S221509862100046X
+ * @param {Number[]}  arr   Array to search. Must be sorted low to high.
+ * @param {Object} obj      Object to find.
+ * @param {Function} valuationFn  How to value each object in the array.
+ *                                Must be ordered comparable to the sort
+ * @return {Number|-1} Index of the value or -1 if not found
+ */
+export function interpolateBinaryFindIndexBeforeObject(arr, obj, valuationFn = a => a) {
   let left = 0;
   let right = arr.length - 1;
   const x = valuationFn(obj);
 
-  if(x > valuationFn(arr[right])) { return right; }
-  if(x < valuationFn(arr[0])) { return -1; }
+  if (x > valuationFn(arr[right])) { return right; }
+  if (x < valuationFn(arr[0])) { return -1; }
 
-  while(left < right) {
+  while (left < right) {
     const v_left = valuationFn(arr[left]);
     const v_right = valuationFn(arr[right]);
-    if(x < v_left || x > v_right) { break; }
+    if (x < v_left || x > v_right) { break; }
 
     const inter = left + Math.ceil((x - v_left) / (v_right - v_left) * (right - left));
-    const v_inter = valuationFn(arr[inter])
+    const v_inter = valuationFn(arr[inter]);
 
-    if(x === v_inter) {
+    if (x === v_inter) {
       return inter - 1;
-    } else if(x > v_inter) {
-      const mid = Math.floor((inter + right) / 2)
-      if(x <= valuationFn(arr[mid])) {
+    } else if (x > v_inter) {
+      const mid = Math.floor((inter + right) / 2);
+      if (x <= valuationFn(arr[mid])) {
         left = inter + 1;
         right = mid;
       } else {
         left = mid + 1;
       }
-    } else { //if(x < arr[inter]) { // if(x < arr[inter])
+    } else {
       const mid = Math.floor((inter + left) / 2);
-      if(x >= valuationFn(arr[mid])) {
+      if (x >= valuationFn(arr[mid])) {
         left = mid;
         right = inter - 1;
       } else {
@@ -510,60 +477,8 @@ export function interpolateBinaryFindIndexBeforeObject(arr, obj, valuationFn = (
     }
   }
 
-
-  if(x > valuationFn(arr[left - 1]) && x < valuationFn(arr[left])) { return left - 1; }
-  if(x > valuationFn(arr[right]) && x < valuationFn(arr[right + 1])) { return right; }
+  if (x > valuationFn(arr[left - 1]) && x < valuationFn(arr[left])) { return left - 1; }
+  if (x > valuationFn(arr[right]) && x < valuationFn(arr[right + 1])) { return right; }
 
   return -1;
 }
-
-
-// export function interpolateBinaryFindIndexAfterObject(arr, obj, valuationFn = (a) => a) {
-//   let left = 0;
-//   let right = arr.length - 1;
-//   const x = valuationFn(obj);
-//
-//   if(x > valuationFn(arr[right])) { return right; }
-//   if(x < valuationFn(arr[0])) { return -1; }
-//
-//   while(left < right) {
-//     const v_left = valuationFn(arr[left]);
-//     const v_right = valuationFn(arr[right]);
-//     if(x < v_left || x > v_right) { break; }
-//
-//     const inter = left + Math.ceil((x - v_left) / (v_right - v_left) * (right - left));
-//     const v_inter = valuationFn(arr[inter])
-//
-//     if(x === v_inter) {  // use while loop to walk forward/backward based on whether this is before or after?
-//       return inter - 1;
-//     } else if(x > v_inter) {
-//       const mid = Math.floor((inter + right) / 2)
-//       if(x <= valuationFn(arr[mid])) {
-//         left = inter + 1;
-//         right = mid;
-//       } else {
-//         left = mid + 1;
-//       }
-//     } else { //if(x < arr[inter]) { // if(x < arr[inter])
-//       const mid = Math.floor((inter + left) / 2);
-//       if(x >= valuationFn(arr[mid])) {
-//         left = mid;
-//         right = inter - 1;
-//       } else {
-//         right = mid - 1;
-//       }
-//     }
-//   }
-//
-//
-//   if(x > valuationFn(arr[left - 1]) && x < valuationFn(arr[left])) { return left - 1; }
-//   if(x > valuationFn(arr[right]) && x < valuationFn(arr[right + 1])) { return right; }
-//
-//   return -1;
-// }
-
-
-
-
-
-
