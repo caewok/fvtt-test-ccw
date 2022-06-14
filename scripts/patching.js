72ebe27c61fd579786aed30ee73ce09590173ba1
Michael Enion <caewok_2000@yahoo.com>
2022-06-14 07:41:52 -0700

    Add set intersect method

--- scripts/patching.js
+++ scripts/patching.js
@@ -13,8 +13,16 @@ export function registerLibWrapperMethods() {
 
 export function patchHelperMethods() {
   function setUnion(b) { return new Set([...this, ...a]); }
+  function setIntersect(b) { return  new Set([...this].filter(x => b.has(x))); }
+
   Object.defineProperty(Set.prototype, "union", {
     value: setUnion,
+    writable: true,
+    configurable: true
+  });
+
+  Object.defineProperty(Set.prototype, "intersect", {
+    value: setIntersect,
     writable: true,
     configurable: true
   });
